2ca7c30dcdbe8fc9a1e026d74ac125cc86ddf497
Prad <prnk28@gmail.com>
2020-11-01 19:38:08 -0500

    Cleanup

--- bind/core/dnsconfig.go
+++ bind/core/dnsconfig.go
@@ -1,13 +1,8 @@
-// from: https://gist.github.com/cs8425/107e01a0652f1f1f6e033b5b68364b5e
-
-//nolint
 package core
 
 import (
-	_ "net"
 	"sync"
 	"time"
-	_ "unsafe"
 )
 
 // DNS resolve workaround for Android & iOS in pure go
