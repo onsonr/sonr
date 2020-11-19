d7ce0dab3e566bdf7e17b74c1e92cba910d56d0d
Prad <prnk28@gmail.com>
2020-11-19 00:42:39 -0500

    Handle TCP Connection with json string

--- bind/auth.go
+++ bind/auth.go
@@ -3,11 +3,11 @@ package sonr
 import (
 	"bufio"
 	"fmt"
-
-	"io/ioutil"
+	"io"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	pb "github.com/sonr-io/core/pkg/models"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -41,70 +41,106 @@ func (sn *Node) NewAuthStream(stream network.Stream) {
 
 // ^ Write Message on Stream ^ //
 func (asc *authStreamConn) Write(authMsg *pb.AuthMessage) error {
-	// Initialize
+	// Initialize Writer
 	writer := bufio.NewWriter(asc.stream)
 	fmt.Println("Auth Msg Struct: ", authMsg)
-	marshaledBytes, err := proto.Marshal(authMsg)
+
+	// Convert to String
+	json, err := protojson.Marshal(authMsg)
 	if err != nil {
-		fmt.Println("Error Marshalling bytes: ", err)
+		fmt.Println("Error Marshalling json: ", err)
 	}
 
-	bytesWritten, err := writer.Write(marshaledBytes)
+	// Log Conversion
+	println("Auth Msg Bytes: ", json)
+	fmt.Println("Auth Msg String: ", string(json))
+
+	// Write Message with "Delimiter"=(Seperator for Message Values)
+	bytesWritten, err := writer.WriteString(fmt.Sprintf("%s\n", string(json)))
 	if err != nil {
-		fmt.Println("Write() returned err: ", err)
+		fmt.Println("Error writing to buffer")
 		return err
 	}
+
+	// Log Bytes Written
 	fmt.Println("Number of bytes written: ", bytesWritten)
-	writer.Flush()
+	// Write buffered data
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing buffer")
+		return err
+	}
 	return nil
 }
 
 // ^ Read Data from Msgio ^ //
-func (asc *authStreamConn) Read() {
-	bufReader := bufio.NewReader(asc.stream)
+func (asc *authStreamConn) Read() error {
 	for {
 		// ** Read the Buffer **
-		readBytes, err := ioutil.ReadAll(bufReader)
+		data, err := bufio.NewReader(asc.stream).ReadString('\n')
+		// Connection closed, deregister client
+		if err == io.EOF {
+			return nil
+		}
+		// Buffer Error
 		if err != nil {
-			fmt.Println("Error reading msg: ", err)
-		}
-		fmt.Println("Read Bytes: ", readBytes)
-		authMsg := pb.AuthMessage{}
-		err = proto.Unmarshal(readBytes, &authMsg)
-		if err != nil {
-			fmt.Println("Error unmarshaling msg: ", err)
+			fmt.Println("Error reading from buffer")
+			return err
 		}
 
-		// ** Contains Data **
-		// Construct message
-		if authMsg.Subject != pb.AuthMessage_NONE {
-			// Check Message Subject
-			switch authMsg.Subject {
-			// @ Request to Invite
-			case pb.AuthMessage_REQUEST:
-				// Retreive Values
-				data, err := proto.Marshal(&authMsg)
-				if err != nil {
-					fmt.Println("Error Marshaling RefreshMessage ", err)
-				}
-
-				// Callback the Invitation
-				asc.callback.OnInvited(data)
-
-			// @ Peer Accepted Response to Invite
-			case pb.AuthMessage_ACCEPT:
-				fmt.Println("Auth Accepted")
-				// Callback to Proxies
-				asc.callback.OnResponded(true)
-
-			// @ Peer Accepted Response to Invite
-			case pb.AuthMessage_DECLINE:
-				fmt.Println("Auth Declined")
-				// Callback to Proxies
-				asc.callback.OnResponded(false)
-			}
+		// Empty String
+		if data == "" {
+			return nil
 		}
-		// ! Invalid Subject
-		fmt.Println("Not a subject", authMsg.Subject)
+
+		// End of Message
+		if data == "\n" {
+			return nil
+		}
+
+		// @ Handle it
+		asc.handleMessage(data)
 	}
+}
+
+// ^ Handle Received Message ^ //
+func (asc *authStreamConn) handleMessage(data string) {
+	// Convert Bytes to Json
+	fmt.Println("Json String: ", data)
+	authMsg := pb.AuthMessage{}
+	err := protojson.Unmarshal([]byte(data), &authMsg)
+	if err != nil {
+		fmt.Println("Error unmarshaling msg into json: ", err)
+	}
+
+	// ** Contains Data **
+	if authMsg.Subject != pb.AuthMessage_NONE {
+		// Check Message Subject
+		switch authMsg.Subject {
+		// @ Request to Invite
+		case pb.AuthMessage_REQUEST:
+			// Retreive Values
+			data, err := proto.Marshal(&authMsg)
+			if err != nil {
+				fmt.Println("Error Marshaling RefreshMessage ", err)
+			}
+
+			// Callback the Invitation
+			asc.callback.OnInvited(data)
+
+		// @ Peer Accepted Response to Invite
+		case pb.AuthMessage_ACCEPT:
+			fmt.Println("Auth Accepted")
+			// Callback to Proxies
+			asc.callback.OnResponded(true)
+
+		// @ Peer Accepted Response to Invite
+		case pb.AuthMessage_DECLINE:
+			fmt.Println("Auth Declined")
+			// Callback to Proxies
+			asc.callback.OnResponded(false)
+		}
+	}
+	// ! Invalid Subject
+	fmt.Println("Not a subject", authMsg.Subject)
 }
