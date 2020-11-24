6f174985f64152d20211aad233980cf3ec63f4ba
Prad <prnk28@gmail.com>
2020-11-23 19:34:31 -0500

    Implement file chunking within data stream

--- internal/stream/data.go
+++ internal/stream/data.go
@@ -3,7 +3,8 @@ package stream
 import (
 	"context"
 	"fmt"
-	"time"
+	"os"
+	"sync"
 
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
@@ -19,11 +20,22 @@ import (
 type OnProgressed func(data []byte)
 type OnComplete func(data []byte)
 
+const BlockSize = 16000
+
 // Struct to Implement Node Callback Methods
 type DataCallback struct {
 	Progressed OnProgressed
 	Completed  OnComplete
 	Error      OnError
+}
+
+// Struct defines a Chunk of Bytes of File
+type Chunk struct {
+	Size    int64
+	Offset  int64
+	Data    []byte
+	Current int64
+	Total   int64
 }
 
 // ^ Struct: Holds/Handles Stream for Authentication  ^ //
@@ -36,6 +48,7 @@ type DataStreamConn struct {
 	data   *pb.Metadata
 	remote *pb.Peer
 	stream network.Stream
+	writer msgio.Writer
 }
 
 // ^ Start New Stream ^ //
@@ -50,13 +63,14 @@ func (dsc *DataStreamConn) Transfer(ctx context.Context, h host.Host, id peer.ID
 	dsc.stream = stream
 	dsc.id = stream.ID()
 	dsc.remote = r
+	dsc.writer = msgio.NewWriter(dsc.stream)
 
 	// Print Stream Info
 	info := stream.Stat()
 	fmt.Println("Stream Info: ", info)
 
 	// Initialize Routine
-	go dsc.writeFileToStream(sm)
+	go dsc.writeFile(sm)
 	return nil
 }
 
@@ -91,6 +105,7 @@ func (dsc *DataStreamConn) read(mrw msgio.ReadCloser) error {
 	}
 
 	dsc.handleBlock(protoMsg)
+	mrw.ReleaseMsg(lengthBytes)
 	return nil
 }
 
@@ -134,40 +149,86 @@ func (dsc *DataStreamConn) sendProgress(current int64, total int64) {
 	dsc.Call.Progressed(bytes)
 }
 
-func (dsc *DataStreamConn) writeFileToStream(sm *sf.SafeMeta) error {
-	// Create Transfer File
-	chunks, err := sf.GetChunks(sm.Metadata())
-	time.Sleep(time.Second)
+func (dsc *DataStreamConn) writeFile(sm *sf.SafeMeta) error {
+	// Get Metadata
+	meta := sm.Metadata()
+	// Open File
+	file, err := os.Open(meta.Path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-	// Manage Output
+	// Number of go routines we need to spawn.
+	concurrency := int(meta.Blocks)
+	// buffer sizes that each of the go routine below should use. ReadAt
+	// returns an error if the buffer size is larger than the bytes returned
+	// from the file.
+	chunksizes := make([]Chunk, concurrency)
+
+	// All buffer sizes are the same in the normal case. Offsets depend on the
+	// index. Second go routine should start at 100, for example, given our
+	// buffer size of 100.
+	for i := 0; i < concurrency; i++ {
+		chunksizes[i].Size = BlockSize
+		chunksizes[i].Offset = int64(BlockSize * i)
+	}
+
+	// check for any left over bytes. Add the residual number of bytes as the
+	// the last chunk size.
+	if remainder := meta.Size % BlockSize; remainder != 0 {
+		c := Chunk{Size: remainder, Offset: int64(concurrency * BlockSize)}
+		concurrency++
+		chunksizes = append(chunksizes, c)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(concurrency)
+
+	for i := 0; i < concurrency; i++ {
+		go func(chunksizes []Chunk, i int) {
+			defer wg.Done()
+
+			// Create Chunk Data
+			chunk := chunksizes[i]
+			chunk.Current = int64(i)
+			chunk.Total = int64(concurrency)
+			chunk.Data = make([]byte, chunk.Size)
+			bytesread, err := file.ReadAt(chunk.Data, chunk.Offset)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Write to Stream
+			dsc.writeChunkToStream(chunk)
+			fmt.Println("bytes read, string(bytestream): ", bytesread)
+			fmt.Println("bytestream to string: ", string(chunk.Data))
+		}(chunksizes, i)
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func (dsc *DataStreamConn) writeChunkToStream(chunk Chunk) {
+	// Create Block Protobuf from Chunk
+	block := &pb.Block{
+		Size:    chunk.Size,
+		Offset:  chunk.Offset,
+		Data:    chunk.Data,
+		Current: chunk.Current,
+		Total:   chunk.Total,
+	}
+	fmt.Println("Block: ", block.String())
+
+	// Convert to bytes
+	bytes, err := proto.Marshal(block)
 	if err != nil {
 		dsc.Call.Error(err, "writeFileToStream")
 	}
 
-	// Initialize Writer
-	writer := msgio.NewWriter(dsc.stream)
-
-	// Iterate through blocks and write to message
-	for _, chunk := range chunks {
-		// Create Block Protobuf from Chunk
-		block := &pb.Block{
-			Size:    chunk.Size,
-			Offset:  chunk.Offset,
-			Data:    chunk.Data,
-			Current: chunk.Current,
-			Total:   chunk.Total,
-		}
-
-		// Convert to bytes
-		bytes, err := proto.Marshal(block)
-		if err != nil {
-			dsc.Call.Error(err, "writeFileToStream")
-		}
-
-		// Add Msg to buffer
-		if err := writer.WriteMsg(bytes); err != nil {
-			return err
-		}
+	// Add Msg to buffer
+	if err := dsc.writer.WriteMsg(bytes); err != nil {
+		dsc.Call.Error(err, "writeFileToStream")
 	}
-	return nil
 }
