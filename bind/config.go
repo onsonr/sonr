800cf589fcfa9c46bf1885b7c0aee357a79dbda4
Prad <prnk28@gmail.com>
2020-12-26 20:22:54 -0500

    Add Worker to Transfer Connection

--- bind/config.go
+++ bind/config.go
@@ -59,7 +59,7 @@ func (sn *Node) setConnection(ctx context.Context) error {
 	log.Println("Lobby Initialized")
 
 	// Initialize Peer Connection
-	if sn.peerConn, err = tf.Initialize(sn.host, sn.pubSub, sn.directories, sn.olc, sn.call.OnInvited, sn.call.OnResponded, sn.call.OnProgress, sn.call.OnReceived, sn.call.OnTransmitted, sn.error); err != nil {
+	if sn.peerConn, err = tf.Initialize(sn.host, &sn.wctx, sn.pubSub, sn.directories, sn.olc, sn.call.OnInvited, sn.call.OnResponded, sn.call.OnProgress, sn.call.OnReceived, sn.call.OnTransmitted, sn.error); err != nil {
 		return err
 	}
 	log.Println("Connection Initialized")
