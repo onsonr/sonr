0d3b163a22b363157015e4ae652c9ca02a052481
Prad <prnk28@gmail.com>
2021-01-11 16:06:07 -0500

    Updated files on 2021-01-11 15:42:08

--- bind/linker.go
+++ bind/linker.go
@@ -1,5 +1,46 @@
 package sonr
 
-func (sn *Node) LinkDevice() {
+import (
+	"log"
 
+	"github.com/skip2/go-qrcode"
+	md "github.com/sonr-io/core/internal/models"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+// ^ Link with a QR Code ^ //
+func (sn *Node) LinkDevice(peerString string) error {
+	// Convert String to Bytes
+	b := []byte(peerString)
+	peer := md.Peer{}
+
+	// Convert to Peer Protobuf
+	err := protojson.Unmarshal(b, &peer)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Save Peer to Disk
+
+	return nil
+}
+
+// ^ Display QR Code of Peer Info ^ //
+func (sn *Node) DisplayCode() []byte {
+	// Get Node JSON
+	jsonBytes, err := protojson.Marshal(sn.peer)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	json := string(jsonBytes)
+	print(json)
+
+	// Encode to QR
+	png, err := qrcode.Encode(json, qrcode.Medium, 256)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return png
 }
