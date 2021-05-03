bb7449d278cd0fbb80a3fbe6a2719a189d4dc152
Prad <prnk28@gmail.com>
2021-05-03 19:51:21 -0400

    Updated files on 2021-05-03 19:30:04

--- internal/network/protocol.go
+++ internal/network/protocol.go
@@ -10,9 +10,7 @@ import (
 // ^ Returns Location from GeoIP ^ //
 func Location(target *md.GeoIP) error {
 	url := "https://find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com/iplocation?apikey=873dbe322aea47f89dcf729dcc8f60e8"
-
 	req, _ := http.NewRequest("GET", url, nil)
-
 	req.Header.Add("x-rapidapi-key", "a09329a7a8mshc7688c2dc3de4f9p197eb4jsn186162847bfb")
 	req.Header.Add("x-rapidapi-host", "find-any-ip-address-or-domain-location-world-wide.p.rapidapi.com")
 
