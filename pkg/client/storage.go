7eac0ff96fabec954100a17c009c1f385617b1d5
Prad <prnk28@gmail.com>
2021-05-18 22:45:28 -0400

    Updated files on 2021-05-18 22:36:19

--- pkg/client/storage.go
+++ pkg/client/storage.go
@@ -3,6 +3,7 @@ package client
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 
 	md "github.com/sonr-io/core/pkg/models"
@@ -30,22 +31,33 @@ func NewUplink(ctx context.Context, appAPIKey string, rootPassword string) (*Sto
 
 // @ Get User from Remote Data Store
 func (s *Storage) GetUser(id string) (*md.User, error) {
-	project, err := uplink.OpenProject(s.ctx, s.appAccess)
+	// Create User Access
+	userAccess, err := s.appAccess.Share(uplink.FullPermission())
+	if err != nil {
+		return nil, err
+	}
+
+	// Open Project
+	project, err := uplink.OpenProject(s.ctx, userAccess)
 	if err != nil {
 		return nil, err
 	}
 	defer project.Close()
 
+	// Marshal USer
 	buffer := new(bytes.Buffer)
 	d, err := project.DownloadObject(s.ctx, "users", id, &uplink.DownloadOptions{})
 	if err != nil {
 		return nil, err
 	}
+
+	// Copy to Buffer
 	_, err = io.Copy(buffer, d)
 	if err != nil {
 		return nil, err
 	}
 
+	// Convert to Object
 	user := &md.User{}
 	err = proto.Unmarshal(buffer.Bytes(), user)
 	if err != nil {
@@ -56,29 +68,37 @@ func (s *Storage) GetUser(id string) (*md.User, error) {
 
 // @ Put User in Remote Data Store
 func (s *Storage) PutUser(u *md.User) error {
-	// Open PRoject
+	// Open Project
 	project, err := uplink.OpenProject(s.ctx, s.appAccess)
 	if err != nil {
-		return err
+		return fmt.Errorf("could not open project: %v", err)
 	}
 	defer project.Close()
 
-	// Marshal Userl
-	bytes, err := proto.Marshal(u)
+	// Ensure the desired Bucket within the Project is created.
+	_, err = project.EnsureBucket(s.ctx, "users")
 	if err != nil {
-		return err
+		return fmt.Errorf("could not ensure bucket: %v", err)
 	}
 
 	// Create Upload Object
-	object, err := project.UploadObject(s.ctx, "users", u.GetId(), &uplink.UploadOptions{})
+	object, err := project.UploadObject(s.ctx, "users", u.GetId(), nil)
 	if err != nil {
 		return err
 	}
 
-	// Write User Bytes to Object
-	_, err = object.Write(bytes)
+	// Marshal User
+	data, err := proto.Marshal(u)
 	if err != nil {
 		return err
+	}
+
+	// Copy to Object
+	buf := bytes.NewBuffer(data)
+	_, err = io.Copy(object, buf)
+	if err != nil {
+		_ = object.Abort()
+		return fmt.Errorf("could not upload data: %v", err)
 	}
 
 	// Push Changes
