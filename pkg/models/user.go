ea6e7ad6bc02a3704c7b7ad63df21777091a6c01
Prad <prnk28@gmail.com>
2021-07-05 23:03:05 -0700

    Updated files on 2021-07-05 20:21:42

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -314,9 +314,6 @@ func (d *Device) WorkingSupportDir() string {
 	if d.IsDesktop() {
 		return d.GetFileSystem().GetLibrary()
 	} else {
-		if d.IsAndroid() {
-			return d.GetFileSystem().GetDocuments()
-		}
 		return d.GetFileSystem().GetSupport()
 	}
 }
