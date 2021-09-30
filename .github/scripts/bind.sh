8b255708e58ef77d1fe165c9279a2a5e820baf27
Prad <prnk28@gmail.com>
2021-09-30 14:26:36 -0700

    🔨 (buildscripts): Implemented SH Bind Script for iOS and Android

--- .github/scripts/bind.sh
+++ .github/scripts/bind.sh
@@ -10,70 +10,131 @@ PLUGIN_ANDROID=${ROOT_DIR}/plugin/android/libs
 PLUGIN_IOS=${ROOT_DIR}/plugin/ios/Frameworks
 ANDROID_ARTIFACT=io.sonr.core.aar
 IOS_ARTIFACT=Core.xcframework
+
+platform=""
+output=""
+
+# Functions for printing help
+usage() {                                 # Function: Print a help message.
+  echo ""
+  echo "🚨  ERROR: Invalid Parameters"
+  echo "Usage: sonr-io/core/bind.sh [ -p PLATFORM (ios, android) ] [ -o OUTPUT (plugin, build) ]" 1>&2
+  echo ""
+}
+exit_abnormal() {                         # Function: Exit with error.
+  usage
+  exit 1
+}
 cd ${PROJECT_DIR}
+
+# Parse arguments
+while getopts ":p:o:" options; do         # Loop: Get the next option;
+  case "${options}" in                    #
+    p)                                    # If the option is n,
+      platform=${OPTARG}                      # set $NAME to specified value.
+      ;;
+    o)                                    # If the option is t,
+      output=${OPTARG}                     # Set $TIMES to specified value.
+      ;;
+    :)                                    # If expected argument omitted:
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal                       # Exit abnormally.
+      ;;
+    *)                                    # If unknown (any other) option:
+      exit_abnormal                       # Exit abnormally.
+      ;;
+  esac
+done
 
 echo ""
 echo "⚡️ Sonr Bind Script for Mobile"
 echo ""
-echo "1. Choose Platform (ios, android)"
-read -p "Platform: " platform
+if [ "${platform}" == "" ]; then
+    echo "1. Choose Platform (ios, android)"
+    read -p "Platform: " platform
+else
+    echo " └─ Building for [ ${platform} ] 🔥 "
+fi
+if [ "${output}" == "" ]; then
+    echo "2. Choose Output Path (plugin, build)"
+    read -p "Output Path: " output
+else
+    echo " └─ Output to [ ${output} ] 📩 "
+fi
 echo ""
-echo "2. Choose Output Path (plugin, build)"
-read -p "Output Path: " output
 
-echo "- (1/3) Installing Dependencies [ ~2min ]"
+echo "- (1/4) Installing Dependencies [ ~2min ]"
 go get -u golang.org/x/mobile/bind
-
 if [ $platform == android ]; then
-    echo "- (2/3) Binding Java AAR 🤖 [ >8min ]"
+    echo "- (2/4) Binding Java AAR 🤖 [ >8min ]"
     cd ${CORE_BIND_DIR}
     gomobile init
 elif [ $platform == ios ]; then
-    echo "- (2/3) Binding Objective-C Framework 📱 [ >8min ]"
+    echo "- (2/4) Binding Objective-C Framework 📱 [ >8min ]"
+    cd ${CORE_BIND_DIR}
+    gomobile init
+elif [ $platform == all ]; then
+    echo "- (2/4) Binding All Frameworks 🌎  [ >16min ]"
     cd ${CORE_BIND_DIR}
     gomobile init
 else
-    echo "Invalid platform"
-    exit 1
+    exit_abnormal
 fi
 
 echo ""
 echo "----------------- (Build Output) -------------------"
 if [ $platform == android ]; then
     if [ $output == plugin ]; then
-        gtime gomobile bind -ldflags='-s -w' -v -target=android -o ${PLUGIN_ANDROID}/${ANDROID_ARTIFACT}
+        gtime -q gomobile bind -ldflags='-s -w' -v -target=android -o ${PLUGIN_ANDROID}/${ANDROID_ARTIFACT}
         echo ""
     elif [ $output == build ]; then
         mkdir -p ${PROJECT_DIR}/build
-        gtime gomobile bind -ldflags='-s -w' -v -target=android -o ${PROJECT_DIR}/build/${ANDROID_ARTIFACT}
+        gtime -q gomobile bind -ldflags='-s -w' -v -target=android -o ${PROJECT_DIR}/build/${ANDROID_ARTIFACT}
         echo ""
     else
-        echo "Invalid output"
-        exit 1
+        exit_abnormal
     fi
 elif [ $platform == ios ]; then
     if [ $output == plugin ]; then
-        gtime gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PLUGIN_IOS}/${IOS_ARTIFACT}
+        gtime -q --format=%E gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PLUGIN_IOS}/${IOS_ARTIFACT}
         echo ""
     elif [ $output == build ]; then
         mkdir -p ${PROJECT_DIR}/build
-        gtime gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PROJECT_DIR}/build/${IOS_ARTIFACT}
+        gtime -q --format=%E gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PROJECT_DIR}/build/${IOS_ARTIFACT}
         echo ""
     else
-        echo "Invalid output"
-        exit 1
+        exit_abnormal
+    fi
+elif [ $platform == all ]; then
+    if [ $output == plugin ]; then
+        echo "Building iOS (3/4)"
+        gtime -q --format=%E gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PLUGIN_IOS}/${IOS_ARTIFACT}
+        echo ""
+        echo "Building Android (4/4)"
+        gtime -q gomobile bind -ldflags='-s -w' -v -target=android -o ${PLUGIN_ANDROID}/${ANDROID_ARTIFACT}
+        echo ""
+    elif [ $output == build ]; then
+        mkdir -p ${PROJECT_DIR}/build
+        echo "Building iOS (3/4)"
+        gtime -q --format=%E gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PROJECT_DIR}/build/${IOS_ARTIFACT}
+        echo ""
+        echo "Building Android (4/4)"
+        gtime -q --format=%E gomobile bind -ldflags='-s -w' -v -target=ios/arm64 -bundleid=io.sonr.core -o ${PROJECT_DIR}/build/${IOS_ARTIFACT}
+        echo ""
+    else
+        exit_abnormal
     fi
 else
-    exit 1
+    exit_abnormal
 fi
-
+echo ""
 go mod tidy
 
 if [ $platform == android ]; then
-    echo "✅  Finished Binding for Android ➡ `date`"
+    echo "✅  Finished Binding for Android"
 elif [ $platform == ios ]; then
-    echo "✅  Finished Binding for iOS ➡ `date`"
+    echo "✅  Finished Binding for iOS"
 else
-    exit 1
+    echo "✅  Finished Binding for All Platforms"
 fi
-
+exit 0
