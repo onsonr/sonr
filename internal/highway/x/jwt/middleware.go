b2f5128cb8ff2f71994a5047f9867ec0b2699725
josh long <josh@sonr.io>
2022-05-12 16:43:21 -0400

    update to authorization middleware to parse header

--- internal/highway/x/jwt/middleware.go
+++ internal/highway/x/jwt/middleware.go
@@ -6,8 +6,13 @@ import (
 
 type JWTParserMiddleware = func() error
 
-func (j *JWT) BuildJWTParseMiddleware(tokenString string) JWTParserMiddleware {
+func (j *JWT) BuildJWTParseMiddleware(headers string) JWTParserMiddleware {
 	return func() error {
+		tokenString, err := ExtractBearerToken(headers)
+		if err != nil {
+			return errors.New("Unable to parse headers")
+		}
+
 		token, err := j.Parse(tokenString)
 		if err != nil {
 			return err
