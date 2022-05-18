ef8d00260c614a3400251156b92c02f7b25dbf4a
brokecollegedevwithaclothingobsession <supremehitsheads@gmail.com>
2022-05-18 15:46:10 -0700

    added check if alias does not exist and txn

--- x/registry/keeper/msg_server_transfer_alias.go
+++ x/registry/keeper/msg_server_transfer_alias.go
@@ -11,31 +11,45 @@ import (
 func (k msgServer) TransferAlias(goCtx context.Context, msg *types.MsgTransferAlias) (*types.MsgTransferAliasResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// Check if Alias exists
+	// Check if Alias exists if not return error
 	_, aliasIsFound := k.GetWhoIsFromAlias(ctx, msg.GetAlias())
-	// If a name is found in store
-	if aliasIsFound {
-		return nil, sdkerrors.Wrap(types.ErrAliasUnavailable, "Name already has an owner")
+	// If a name is not found in store return error
+	if !aliasIsFound {
+		return nil, sdkerrors.Wrap(types.ErrAliasUnavailable, "Name does not exist")
 	}
 
-	// Get whois from controller
-	whois, isFound := k.GetWhoIsFromController(ctx, msg.Creator)
+	// Get whois from Owner
+	// TODO: Implement Multisig for root level owner
+	whois, isFound := k.GetWhoIsFromAlias(ctx, msg.Creator)
 	if !isFound {
 		return nil, sdkerrors.Wrapf(types.ErrControllerNotFound, "creator %s", msg.Creator)
 	}
 
-	// Check if alias is sold
+	// Find associated Alias in whoIs
 	idx, alias, err := whois.FindAliasByName(msg.GetAlias())
 	if err != nil {
 		return nil, sdkerrors.Wrapf(types.ErrAliasNotFound, "alias %s", msg.GetAlias())
 	}
 
-	// Transfer alias from whois to new owner
+	// Convert Alias Owner address strings to sdk.AccAddress
+	aliasOwner, err := sdk.AccAddressFromBech32(msg.GetRecipient())
+	if err != nil {
+		return nil, sdkerrors.Wrapf(err, "alias owner %s", msg.GetRecipient())
+	}
+
+	//TODO: put this in escrow to mitigate transfer/alias race condition attacks
+
+	// Send Coins to new owner
+	err = k.bankKeeper.SendCoins(ctx, aliasOwner, aliasOwner, sdk.NewCoins(sdk.NewCoin("snr", sdk.NewInt(int64(msg.GetAmount())))))
+	if err != nil {
+		return nil, sdkerrors.Wrapf(err, "failed to send coins to %s", msg.GetRecipient())
+	}
+
+	// Transfer Alias to new owner if transaction is successful
 	msg.Recipient = msg.Creator
 	whois.Alias[idx] = alias
 
-	// Update WhoIs in Keeper store
-	k.SetWhoIs(ctx, whois)
+	// Update WhoIs in keeper store
 	return &types.MsgTransferAliasResponse{
 		Success: true,
 		WhoIs:   &whois,
