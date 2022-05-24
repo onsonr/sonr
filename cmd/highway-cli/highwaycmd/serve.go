5be2c67962eade2f43ff6123e7bf6b904b85ec2b
Prad Nukala <pradn@sonr.io>
2022-05-23 20:56:22 -0400

    Fix blockchain serve cmd

--- cmd/highway-cli/highwaycmd/serve.go
+++ cmd/highway-cli/highwaycmd/serve.go
@@ -16,10 +16,6 @@ func bootstrapServeCommand(ctx context.Context) (serveCmd *cobra.Command) {
 		RP_SERVER_PORT = viper.GetString("RP_SERVER_PORT")
 		GRPC_PORT      = viper.GetInt("GRPC_PORT")
 		HTTP_PORT      = viper.GetInt("HTTP_PORT")
-		DISPLAY_NAME   = viper.GetString("DISPLAY_NAME")
-		RP_ID          = viper.GetString("RP_ID")
-		RP_AUTH_ORIGIN = viper.GetString("RP_AUTH_ORIGIN")
-		IS_DEBUG       = viper.GetBool("IS_DEBUG")
 	)
 
 	logger := golog.Default.Child("serve")
@@ -34,7 +30,7 @@ func bootstrapServeCommand(ctx context.Context) (serveCmd *cobra.Command) {
 			node, err := highway.NewHighway(
 				ctx,
 				config.WithHighwayAPISettings("tcp", "localhost", GRPC_PORT, HTTP_PORT),
-				config.WithWebAuthnConfig(DISPLAY_NAME, RP_ID, RP_AUTH_ORIGIN, IS_DEBUG))
+				nil)
 			cobra.CheckErr(err)
 
 			node.Serve()
