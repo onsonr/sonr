f30d3e447e53d9c7f6ad436090b6f2a164a45283
josh long <josh@sonr.io>
2022-05-26 12:19:51 -0400

    addition of middleware loading abstraction

--- internal/highway/api/middleware.go
+++ internal/highway/api/middleware.go
@@ -1,26 +1,52 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-var middlewareDefs []gin.HandlerFunc = nil
+	"github.com/gin-gonic/gin"
+)
+
+type HighwayMiddleware struct {
+	definition gin.HandlerFunc // middleware definition
+	disabled   bool            // should be loaded on registration
+}
+
+type UnsignedResponse struct {
+	Message interface{} `json:"message"`
+}
+
+var middlewareDefs []HighwayMiddleware = nil
 
 func (s *HighwayServer) RegisterMiddleWare() {
 	// Add middleware here to be loaded into Highway's Router
-	middlewareDefs = []gin.HandlerFunc{
-		func(ctx *gin.Context) {
-			token := ctx.GetHeader("Authorization")
-			if token != "" {
+	middlewareDefs = []HighwayMiddleware{
+		{
+			definition: func(ctx *gin.Context) {
+				token := ctx.GetHeader("Authorization")
+				if token == "" {
+					ctx.AbortWithStatusJSON(http.StatusBadRequest, UnsignedResponse{
+						Message: "Authorization token not found",
+					})
+				}
 				error := s.JwtToken.BuildJWTParseMiddleware(token)()
 
 				if error != nil {
 					logger.Errorf("Error while processing authorization header: %s", error.Error())
+					ctx.AbortWithStatusJSON(http.StatusBadRequest, UnsignedResponse{
+						Message: error.Error(),
+					})
+					return
 				}
+
 				ctx.Next()
-			}
+			},
+			disabled: false,
 		},
 	}
 
 	for _, mw := range middlewareDefs {
-		s.Router.Use(mw)
+		if !mw.disabled {
+			s.Router.Use(mw.definition)
+		}
 	}
 }
