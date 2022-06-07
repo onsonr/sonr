ea39a389c29171500505d849c93f32577dbdac4e
Brayden Cloud <brayden14cloud@gmail.com>
2022-06-07 11:36:37 -0500

    make simulation tests run

--- x/schema/handler.go
+++ x/schema/handler.go
@@ -11,12 +11,19 @@ import (
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
 	// this line is used by starport scaffolding # handler/msgServer
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
+		case *types.MsgCreateSchema:
+			res, err := msgServer.CreateSchema(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgDeprecateSchema:
+			res, err := msgServer.DeprecateSchema(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
