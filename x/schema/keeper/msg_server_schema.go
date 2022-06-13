ebe6eb18ec31486e21966b6621c7e2db9ab4b8ab
Andy <andy@Andys-MBP.lan>
2022-06-13 11:43:51 -0500

    Added DeprecateSchema(), added isActive to Schema proto. No responses/errors implemented.

--- x/schema/keeper/msg_server_schema.go
+++ x/schema/keeper/msg_server_schema.go
@@ -47,5 +47,37 @@ func (k msgServer) CreateSchema(goCtx context.Context, msg *types.MsgCreateSchem
 
 func (k msgServer) DeprecateSchema(goCtx context.Context, msg *types.MsgDeprecateSchema) (*types.MsgDeprecateSchemaResponse, error) {
 	// TODO: implement
-	return nil, errors.New("unimplemented")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	err := msg.ValidateBasic()
+	if err != nil {
+		return nil, err
+	}
+
+	/*oldSchema*/
+	oldSchema, found := k.GetSchema(ctx, msg.GetDid()) //TODO: Get ID of schema
+
+	if found {
+
+		//Creator guard
+		creatorDid, creatorErr := did.ParseDID(msg.GetDid())
+		oldSchemaDid, oldErr := did.ParseDID(oldSchema.GetDid())
+		if creatorErr != nil {
+			return nil, creatorErr //TODO: Create response
+		} else if oldErr != nil {
+			return nil, oldErr //TODO: Create response
+		} else if creatorDid != oldSchemaDid {
+			return nil, errors.New("Permission Denied: Only the owner may deprecate a schema") //TODO: Create response, error
+		}
+
+		//If already deactivated, do nothing.
+		//Responsibility of caller to check if isActive beforehand
+		if oldSchema.IsActive {
+			oldSchema.IsActive = false
+			k.SetSchema(ctx, oldSchema)
+		}
+
+		return nil, nil //TODO: Create response
+	} else {
+		return nil, errors.New("Schema not found") //TODO: Create response, error
+	}
 }
