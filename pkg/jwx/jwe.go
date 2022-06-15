b4365b3234f67c8f547c0d38d08b8f4f694293c1
josh long <josh@sonr.io>
2022-06-15 18:03:35 -0400

    update to jwe to return encrypted and decrypted values

--- pkg/jwx/jwe.go
+++ pkg/jwx/jwe.go
@@ -5,11 +5,11 @@ import "github.com/lestrrat-go/jwx/v2/jwe"
 func (x *jwxImpl) EncryptJWE(payload []byte, opts ...jwe.EncryptOption) ([]byte, error) {
 	_opts := make([]jwe.EncryptOption, len(opts)+1)
 	_opts[len(opts)] = jwe.WithKey(x.sigAlg, x.key)
-	jwe.Encrypt(payload, _opts...)
+	return jwe.Encrypt(payload, _opts...)
 }
 
 func (x *jwxImpl) DecryptJWE(payload []byte, opts ...jwe.DecryptOption) ([]byte, error) {
 	_opts := make([]jwe.DecryptOption, len(opts)+1)
 	_opts[len(opts)] = jwe.WithKey(x.sigAlg, x.key)
-	jwe.Decrypt(payload, _opts...)
+	return jwe.Decrypt(payload, _opts...)
 }
