e37ad1b6c2536d877859c1efe885ffb32e9aeeaa
josh long <josh@sonr.io>
2022-06-15 16:45:09 -0400

    migration of test generation function to testutils

--- testutil/keeper/schema.go
+++ testutil/keeper/schema.go
@@ -1,7 +1,10 @@
 package keeper
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -52,4 +55,27 @@ func SchemaKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 	k.SetParams(ctx, types.DefaultParams())
 
 	return k, ctx
+}
+
+func CreateWhatIsWithDID(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.WhatIs {
+	items := make([]types.WhatIs, n)
+	var offset int = n + 1
+	for i := range items {
+		id := fmt.Sprintf("did:snr:%s", strconv.Itoa(i))
+		schemaId := fmt.Sprintf("did:snr:%s", strconv.Itoa(offset))
+		var whatIs = types.WhatIs{
+			Did: id,
+			Schema: &types.SchemaReference{
+				Did:   schemaId,
+				Label: "test",
+				Cid:   strconv.Itoa(i),
+			},
+			Timestamp: time.Now().Unix(),
+			IsActive:  true,
+		}
+		keeper.SetWhatIs(ctx, whatIs)
+		items[i] = whatIs
+		offset += 1
+	}
+	return items
 }
