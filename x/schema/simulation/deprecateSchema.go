4914560d22687f1181f3d47077b888f5da16fafe
Andy <andy@Andys-MBP.lan>
2022-06-16 19:15:30 -0500

    Preliminary simulation

---
+++ x/schema/simulation/deprecateSchema.go
@@ -0,0 +1,41 @@
+package simulation
+
+import (
+	"math/rand"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+	"github.com/sonr-io/sonr/x/schema/keeper"
+	"github.com/sonr-io/sonr/x/schema/types"
+)
+
+func SimulateMsgDeprecateSchema(ak types.AccountKeeper, bk types.BankKeeper, k keeper.Keeper) simtypes.Operation {
+	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		simAccount, _ := simtypes.RandomAcc(r, accs) //TODO: Create schemas first
+		deprMsg := types.MsgDeprecateSchema{
+			Creator: simAccount.Address.String(), //TODO: find
+			Did:     "",                          //TODO: add
+		}
+
+		txCtx := simulation.OperationInput{
+			R:               r,
+			App:             app,
+			TxGen:           simappparams.MakeTestEncodingConfig().TxConfig,
+			Cdc:             nil,
+			Msg:             &deprMsg,
+			MsgType:         types.TypeMsgDeprecateSchema, //deprMsg.Type(), //Figure out discrepancy
+			Context:         ctx,
+			SimAccount:      simAccount,
+			ModuleName:      types.ModuleName,
+			CoinsSpentInMsg: sdk.NewCoins(),
+			AccountKeeper:   ak,
+			Bankkeeper:      bk,
+		}
+
+		return simulation.GenAndDeliverTxWithRandFees(txCtx)
+	}
+}
