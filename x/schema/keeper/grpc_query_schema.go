5d9ef531ed66db81c08556da52d66d20f1333620
josh long <josh@sonr.io>
2022-06-23 20:19:47 -0400

    update to query schema definition of retrieving content from persistence

--- x/schema/keeper/grpc_query_schema.go
+++ x/schema/keeper/grpc_query_schema.go
@@ -5,25 +5,41 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/sonr-io/sonr/x/schema/types"
+	st "github.com/sonr-io/sonr/x/schema/types"
 )
 
-func (k Keeper) QuerySchema(goCtx context.Context, req *types.QuerySchemaRequest) (*types.QuerySchemaResponse, error) {
+func (k Keeper) QuerySchema(goCtx context.Context, req *st.QuerySchemaRequest) (*st.QuerySchemaResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	ref, found := k.GetWhatIsFromCreator(ctx, req.Creator)
 	if !found || len(ref) < 1 {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "Schema was not found")
 	}
 
+	var what_is *st.WhatIs
 	for _, item := range ref {
 		if item.Did == req.Did {
-			var resp = types.QuerySchemaResponse{
-				Schema: &item,
-			}
-
-			return &resp, nil
+			what_is = &item
+			break
 		}
 	}
 
-	return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "Schema was not found")
+	if what_is == nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrNotFound, "Schema was not found for id: %s", req.Did)
+	}
+
+	var schemaJson *map[string]st.SchemaKind
+	err := k.LookUpContent(what_is.Schema.Cid, &schemaJson)
+
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "Error while accessing schema content")
+	}
+	var definition *st.SchemaDefinition = &st.SchemaDefinition{
+		Creator: what_is.Creator,
+		Label:   what_is.Schema.Label,
+		Fields:  *schemaJson,
+	}
+	return &st.QuerySchemaResponse{
+		Schema:     what_is,
+		Definition: definition,
+	}, nil
 }
