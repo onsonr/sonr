832c034d87dbf6399f45cf1953a1b0ac95cf3f2f
josh long <josh@sonr.io>
2022-07-07 18:01:55 -0400

    addition of testing utility for creating a schema on chain

--- internal/motor/x/schemas/utils.go
+++ internal/motor/x/schemas/utils.go
@@ -1,7 +1,14 @@
 package schemas
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ipld/go-ipld-prime/datamodel"
+	"github.com/sonr-io/sonr/pkg/tx"
 	st "github.com/sonr-io/sonr/x/schema/types"
 )
 
@@ -27,4 +34,44 @@ func (as *appSchemaInternalImpl) GetNodeMap() map[string]datamodel.Node {
 
 func (as *appSchemaInternalImpl) GetWhatIsMap() map[string]*st.WhatIs {
 	return as.WhatIs
+}
+
+func (as *appSchemaInternalImpl) Broadcast(addr string, msg sdk.Msg) ([]byte, error) {
+	reqTx, err := tx.BuildTx(as.Acct, addr, msg)
+	if err != nil {
+		return nil, err
+	}
+	req, err := reqTx.Marshal()
+	if err != nil {
+		return nil, err
+	}
+
+	apiEndpoint := "http://127.0.0.1:1317/cosmos/tx/v1beta1/txs"
+	values := map[string]interface{}{
+		"description": addr,
+		"tx_bytes":    req,
+		"mode":        "BROADCAST_MODE_BLOCK",
+	}
+
+	jsonBytes, err := json.Marshal(values)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.Post(apiEndpoint, "application/json", bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+	bytes, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	respJson := make(map[string]interface{})
+	json.Unmarshal(bytes, &respJson)
+	return bytes, nil
 }
