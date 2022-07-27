f25e4119deee5f0265b718f8e70152d1bddda728
drgomesp <drgomesp@gmail.com>
2022-07-27 13:28:05 -0300

    exposed dagput and dagget on ipfs.Protocol interface

--- pkg/protocol/ipfs.go
+++ pkg/protocol/ipfs.go
@@ -3,6 +3,8 @@ package protocol
 import "context"
 
 type IPFS interface {
+	DagGet(ref string, out interface{}) error
+	DagPut(data interface{}, inputCodec, storeCodec string) (string, error)
 	LookUpData(ctx context.Context, cid string, data interface{}) error
 	GetData(ctx context.Context, cid string) ([]byte, error)
 	PutData(ctx context.Context, data []byte) (string, error)
--- pkg/protocol/ipfs_shell.go
+++ pkg/protocol/ipfs_shell.go
@@ -12,16 +12,26 @@ import (
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
+var _ IPFS = (*IPFSShell)(nil)
+
 // IPFSShell implements a protocol.IPFS featuring:
 // 	- IPFS initialization and error fallback
 //	- In-memory cache mechanism
 type IPFSShell struct {
-	*shell.Shell
+	shell *shell.Shell
 	cache datastore.Datastore
 }
 
 func NewIPFSShell(url string, cacheStore datastore.Datastore) *IPFSShell {
-	return &IPFSShell{Shell: shell.NewShell(url), cache: cacheStore}
+	return &IPFSShell{shell: shell.NewShell(url), cache: cacheStore}
+}
+
+func (i *IPFSShell) DagGet(ref string, out interface{}) error {
+	panic("TODO")
+}
+
+func (i *IPFSShell) DagPut(data interface{}, inputCodec, storeCodec string) (string, error) {
+	panic("TODO")
 }
 
 func (i *IPFSShell) LookUpData(ctx context.Context, cid string, data interface{}) error {
@@ -32,27 +42,26 @@ func (i *IPFSShell) LookUpData(ctx context.Context, cid string, data interface{}
 
 	if exists {
 		return nil
-	} else {
-		//timestamp := fmt.Sprintf("%d", time.Now().Unix())
-		outPath := filepath.Join(os.TempDir(), cid+".txt")
+	}
 
-		defer os.Remove(outPath)
+	outPath := filepath.Join(os.TempDir(), cid+".txt")
 
-		if err := i.Get(cid, outPath); err != nil {
-			return err
-		}
+	defer os.Remove(outPath)
 
-		buf, err := os.ReadFile(outPath)
-		if err != nil {
-			return err
-		}
-
-		if err = json.Unmarshal(buf, &data); err != nil {
-			return err
-		}
-
+	if err := i.shell.Get(cid, outPath); err != nil {
 		return err
 	}
+
+	buf, err := os.ReadFile(outPath)
+	if err != nil {
+		return err
+	}
+
+	if err = json.Unmarshal(buf, &data); err != nil {
+		return err
+	}
+
+	return err
 }
 
 func (i *IPFSShell) GetData(ctx context.Context, cid string) ([]byte, error) {
@@ -67,7 +76,7 @@ func (i *IPFSShell) GetData(ctx context.Context, cid string) ([]byte, error) {
 	}
 
 	outPath := path.Join("..", "..", "data")
-	if err = i.Get(cid, outPath); err != nil {
+	if err = i.shell.Get(cid, outPath); err != nil {
 		return nil, err
 	}
 
@@ -85,7 +94,7 @@ func (i *IPFSShell) GetData(ctx context.Context, cid string) ([]byte, error) {
 }
 
 func (i *IPFSShell) PutData(ctx context.Context, data []byte) (string, error) {
-	cidStr, err := i.Add(bytes.NewBuffer(data))
+	cidStr, err := i.shell.Add(bytes.NewBuffer(data))
 	if err != nil {
 		return "", err
 	}
@@ -99,9 +108,9 @@ func (i *IPFSShell) PutData(ctx context.Context, data []byte) (string, error) {
 }
 
 func (i *IPFSShell) PinFile(ctx context.Context, cidstr string) error {
-	panic("implement me")
+	panic("TODO")
 }
 
 func (i *IPFSShell) RemoveFile(ctx context.Context, cidstr string) error {
-	panic("implement me")
+	panic("TODO")
 }
