d3fb0c287523b4fe1a322e4edf02dfc8aed0ef90
Nicholas Tindle <nicktindle@outlook.com>
2022-08-02 16:45:24 -0400

    feat: deployment fixes

---
+++ scripts/pair-peers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nodes=( v1-beta v2-beta v3-beta v4-beta)
+
+for i in "${nodes[@]}"; do scp script/sonrd.service root@$(dig "$i".sonr.ws +short):/etc/systemd/system/sonrd.service; done
+
+NODE_ID=sonrd tendermint show-node-id
+EXISTING_PEERS=toml-cli get /root/.sonr/config/config.toml p2p.persistent_peers
+
+toml-cli set /root/.sonr/config/config.toml p2p.persistent_peers
