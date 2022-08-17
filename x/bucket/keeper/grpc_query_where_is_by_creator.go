fb101263ea0d3a7879b6a75a4ea6d1438d75abc5
josh long <josh@sonr.io>
2022-08-17 14:04:27 -0400

    update from merge revert

--- x/bucket/keeper/grpc_query_where_is_by_creator.go
+++ x/bucket/keeper/grpc_query_where_is_by_creator.go
@@ -3,25 +3,45 @@ package keeper
 import (
 	"context"
 
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	bt "github.com/sonr-io/sonr/x/bucket/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/sonr-io/sonr/x/bucket/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) WhereIsByCreator(c context.Context, req *bt.QueryGetWhereIsByCreatorRequest) (*bt.QueryGetWhereIsByCreatorResponse, error) {
+func (k Keeper) WhereIsByCreator(c context.Context, req *types.QueryGetWhereIsByCreatorRequest) (*types.QueryGetWhereIsByCreatorResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
-
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
-	}
-
+	var whereIss []types.WhereIs
 	ctx := sdk.UnwrapSDKContext(c)
-	whereIss := k.GetWhereIsByCreator(ctx, req.Creator)
 
-	return &bt.QueryGetWhereIsByCreatorResponse{
-		WhereIs: whereIss,
+	store := ctx.KVStore(k.storeKey)
+	whereIsStore := prefix.NewStore(store, types.WhereIsKey(types.MemStoreKey))
+
+	pageRes, err := query.Paginate(whereIsStore, req.Pagination, func(key []byte, value []byte) error {
+		var whereIs types.WhereIs
+		if err := k.cdc.Unmarshal(value, &whereIs); err != nil {
+			k.Logger(ctx).Error("Error while unMarshaling WhatIs: %s", err.Error())
+			return err
+		}
+
+		if whereIs.Creator == req.Creator {
+			whereIss = append(whereIss, whereIs)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		k.Logger(ctx).Error("Error while querying whatIs: %s", err.Error())
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryGetWhereIsByCreatorResponse{
+		WhereIs:    whereIss,
+		Pagination: pageRes,
 	}, nil
 }
