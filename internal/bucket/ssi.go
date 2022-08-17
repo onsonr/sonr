ce22a49b83a8c3f820c637b417ce09d8ecbf9015
josh long <josh@sonr.io>
2022-08-17 10:59:09 -0400

    addition of service endpoint creation for top level bucket

--- internal/bucket/ssi.go
+++ internal/bucket/ssi.go
@@ -9,15 +9,11 @@ import (
 
 func (b *bucketImpl) CreateBucketService() did.Service {
 	return did.Service{
-		ID:   ssi.MustParseURI(fmt.Sprintf("v1.sonr.ws:1337/sonr-io/sonr/bucket/where_is/%s/%s", b.adress, b.whereIs.Did)),
+		ID:   ssi.MustParseURI(fmt.Sprintf("v1.sonr.ws:1317/sonr-io/sonr/bucket/where_is/%s/%s", b.adress, b.whereIs.Did)),
 		Type: "Bucket",
 		ServiceEndpoint: map[string]string{
 			"did":     b.whereIs.Did,
 			"creator": b.whereIs.Creator,
 		},
 	}
-}
-
-func (b *bucketImpl) CreateObjectServices() did.Services {
-	return make([]did.Service, 0)
 }
