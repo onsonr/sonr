ec6ac817b5a21bbc1c0ea23fe59ada6cb955b992
Prad Nukala <pradn@sonr.io>
2022-08-18 13:30:34 -0400

    Final clean up and bug fixes for overriding assets

--- scripts/bind.sh
+++ scripts/bind.sh
@@ -15,28 +15,37 @@ while getopts "iaw" opt; do
   case $opt in
     a)
       ANDROID_ARTIFACT=${BUILDDIR}/io.sonr.motor.aar
+      ANDROID_TAR_BALL=${BUILDDIR}/motor-${VERSION}-android.tar.gz
+
       echo "🔷 Binding Android Artifact Version ${VERSION}..."
       cd ${MOTOR_LIB_DIR}
       gomobile bind -ldflags='-s -w' -target=android/arm64 -o ${ANDROID_ARTIFACT} -v
       rm -rf io.sonr.motor-sources.jar
-      tar -czvf ${BUILDDIR}/motor-${VERSION}-android.tar.gz ${ANDROID_ARTIFACT}
+      tar -czvf ${ANDROID_TAR_BALL} ${ANDROID_ARTIFACT}
       rm -rf ${ANDROID_ARTIFACT}
+      echo "✅ Android Tarball written to: ${ANDROID_TAR_BALL}"
       ;;
     i)
       IOS_ARTIFACT=${BUILDDIR}/Motor.xcframework
+      IOS_TAR_BALL=${BUILDDIR}/motor-${VERSION}-ios.tar.gz
+
       echo "🔷 Binding iOS Artifact Version ${VERSION}..."
       cd ${MOTOR_LIB_DIR}
       gomobile bind -ldflags='-s -w' -target=ios -prefix=SNR  -o ${IOS_ARTIFACT} -v
-      tar -czvf ${BUILDDIR}/motor-${VERSION}-ios.tar.gz ${IOS_ARTIFACT}
+      tar -czvf ${IOS_TAR_BALL} ${IOS_ARTIFACT}
       rm -rf ${IOS_ARTIFACT}
+      echo "✅ iOS Tarball written to: ${IOS_TAR_BALL}"
       ;;
     w)
       WASM_ARTIFACT=${BUILDDIR}/sonr-motor.wasm
+      WASM_TAR_BALL=${BUILDDIR}/motor-${VERSION}-wasm.tar.gz
+
       echo "🔷 Binding WebAssembly Artifact Version ${VERSION}..."
       cd ${MOTOR_WASM_DIR}
       GOOS=js GOARCH=wasm go build -tags wasm -o ${WASM_ARTIFACT} -v
       tar -czvf ${BUILDDIR}/motor-${VERSION}-wasm.tar.gz ${WASM_ARTIFACT}
       rm -rf ${WASM_ARTIFACT}
+      echo "✅ WebAssembly Tarball written to: ${WASM_TAR_BALL}"
       ;;
     ?)
       echo "Invalid option: -$OPTARG" >&2
