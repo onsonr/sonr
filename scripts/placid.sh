300fd1cf0ef3e359cfb6cb48ecde41a70ed1b15c
Prad Nukala <pradn@sonr.io>
2022-08-18 17:22:32 -0400

    Fix SH script, remove functions (doesnt work in CI)

--- scripts/placid.sh
+++ scripts/placid.sh
@@ -5,25 +5,17 @@ URL=$2
 JSON_FMT='{ "layers": { "tag": { "text": "version - %s" }, "changelog": { "text": "Full Release notes: %s" } } }'
 JSON=$(printf "$JSON_FMT" "$VERSION" "$URL")
 
-function gen_image (){
-    STAGE_RSP=$(curl -X POST -H "Authorization: ${PLACID_APP_BEARER}" -H "Content-Type: application/json" -d "${JSON}" https://api.placid.app/api/rest/5ijokwc0v )
-    IMAGE_ID=$(echo $STAGE_RSP | jq -r '.id')
-    echo "Queued with image id: $IMAGE_ID"
-    return 0
-}
-
-function download_image (){
-    sleep 5
-    GET_IMAGE_RSP=$(curl -X GET -H "Authorization: ${PLACID_APP_BEARER}" https://api.placid.app/api/rest/images/${IMAGE_ID})
-    echo $GET_IMAGE_RSP
-    IMAGE_URL=$(echo $GET_IMAGE_RSP | jq -r '.image_url')
-    curl $IMAGE_URL -o tmp/cover.png
-    echo "✅ Downloaded image to tmp/cover.png"
-    return 0
-}
-
-echo "🔷 Placid Image Request:"
-echo "\t${JSON}"
+echo "🔷 Placid Image Request:\n\t${JSON}"
+echo "Sending Placid Image Request..."
 mkdir -p tmp
-gen_image
-download_image
+STAGE_RSP=$(curl -X POST -H "Authorization: ${PLACID_APP_BEARER}" -H "Content-Type: application/json" -d "${JSON}" https://api.placid.app/api/rest/5ijokwc0v )
+IMAGE_ID=$(echo $STAGE_RSP | jq -r '.id')
+echo "Queued with image id: $IMAGE_ID"
+
+echo "Waiting for image to be ready before downloading..."
+sleep 5
+GET_IMAGE_RSP=$(curl -X GET -H "Authorization: ${PLACID_APP_BEARER}" https://api.placid.app/api/rest/images/${IMAGE_ID})
+echo $GET_IMAGE_RSP
+IMAGE_URL=$(echo $GET_IMAGE_RSP | jq -r '.image_url')
+curl $IMAGE_URL -o tmp/cover.png
+echo "✅ Downloaded image to tmp/cover.png"
