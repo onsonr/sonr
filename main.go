aacb0d82958dcc5cdfc5b8bcc01a7aff26da28ea
Prad Nukala <pradn@sonr.io>
2022-08-20 19:30:49 -0400

    Remove Deprecated files

--- main.go
+++
@@ -1,9 +0,0 @@
-// DO NOT EDIT
-//
-// <This file SHOULD NOT be deleted nor imported by developers.>
-//
-// ---
-// This file is required by swagger in order to generate the swagger spec for the Highway API. The swagger tool utilizes this root level package in order to
-// be able to search all directories for additional type definitions and other api specs.
-// ---
-package sonr
