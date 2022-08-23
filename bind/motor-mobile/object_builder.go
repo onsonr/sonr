8e5b1ad85859c7ba36d54cf3d65e062f14d7fea8
mcjcloud <brayden14cloud@gmail.com>
2022-08-22 21:20:31 -0500

    split up motor binding layer

---
+++ bind/motor-mobile/object_builder.go
@@ -0,0 +1,131 @@
+package motor
+
+import (
+	"fmt"
+
+	_ "golang.org/x/mobile/bind"
+)
+
+func NewObjectBuilder(name, schemaDid string) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; ok {
+		return fmt.Errorf("object builder exists with name '%s'", name)
+	}
+
+	builder, err := instance.NewObjectBuilder(schemaDid)
+	if err != nil {
+		return err
+	}
+
+	objectBuilders[name] = builder
+	return nil
+}
+
+func SetObjectLabel(name, label string) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	objectBuilders[name].SetLabel(label)
+	return nil
+}
+
+func SetBool(name, fieldName string, v int) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	var value bool
+	if int(v) == 0 {
+		value = false
+	} else {
+		value = true
+	}
+	return objectBuilders[name].Set(fieldName, value)
+}
+
+func SetInt(name, fieldName string, v int) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	value := int(v)
+	return objectBuilders[name].Set(fieldName, value)
+}
+
+func SetFloat(name, fieldName string, v float32) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	value := float32(v)
+	return objectBuilders[name].Set(fieldName, value)
+}
+
+func SetString(name, fieldName, value string) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	return objectBuilders[name].Set(fieldName, value)
+}
+
+func SetBytes(name, fieldName string, v []byte) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	return objectBuilders[name].Set(fieldName, v)
+}
+
+func SetLink(name, fieldName, value string) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if _, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	}
+
+	return objectBuilders[name].Set(fieldName, value)
+}
+
+func RemoveObjectField(name, fieldName string) error {
+	if instance == nil {
+		return errWalletNotExists
+	}
+
+	if builder, ok := objectBuilders[name]; !ok {
+		return fmt.Errorf("no object builder with name '%s'", name)
+	} else {
+		builder.Remove(fieldName)
+	}
+	return nil
+}
