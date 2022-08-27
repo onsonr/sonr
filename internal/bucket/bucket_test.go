869e6f1e5a630b0ab0ab5b5305a2a1cceab78e87
josh long <josh@sonr.io>
2022-08-27 10:09:07 -0400

    testing updates

--- internal/bucket/bucket_test.go
+++ internal/bucket/bucket_test.go
@@ -6,8 +6,13 @@ import (
 
 	shell "github.com/ipfs/go-ipfs-api"
 	"github.com/sonr-io/sonr/internal/bucket"
+	"github.com/sonr-io/sonr/pkg/client"
 	bt "github.com/sonr-io/sonr/x/bucket/types"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	DEV_CHAIN_ADDR = "137.184.190.146:9090"
 )
 
 func CreateMockWhereIs(creator string, content []*bt.BucketItem) *bt.WhereIs {
@@ -22,21 +27,42 @@ func CreateMockWhereIs(creator string, content []*bt.BucketItem) *bt.WhereIs {
 }
 
 func Test_Bucket(t *testing.T) {
-	t.Skip("Skipping in CI")
-	creator := ""
+
+	creator := "snr1ld9u3wpq752wmqaus5rzcfanqg65sgldhnscx5"
+	objectURI := "bafyreihnj3feeesb6wmd46lmsvtwalvuckns647ghy44xn63lfsfed3ydm"
 	s := shell.NewLocalShell()
+	c := client.NewClient(client.ConnEndpointType_DEV)
 	t.Run("Bucket should be defined", func(t *testing.T) {
 		content := []*bt.BucketItem{
 			{
 				Name:      "test",
-				Uri:       "bafyreihnj3feeesb6wmd46lmsvtwalvuckns647ghy44xn63lfsfed3ydm",
+				Uri:       objectURI,
 				Timestamp: time.Now().Unix(),
 				Type:      bt.ResourceIdentifier_CID,
 			},
 		}
 
-		instance := bucket.New(creator, CreateMockWhereIs(creator, content), s, "137.184.190.146:9090")
+		instance := bucket.New(creator, CreateMockWhereIs(creator, content), s, c)
+		assert.NotNil(t, instance)
+	})
+
+	t.Run("Bucket Resolve cid should be in content cache", func(t *testing.T) {
+		content := []*bt.BucketItem{
+			{
+				Name:      "test",
+				Uri:       objectURI,
+				Timestamp: time.Now().Unix(),
+				Type:      bt.ResourceIdentifier_CID,
+			},
+		}
+
+		instance := bucket.New(creator, CreateMockWhereIs(creator, content), s, c)
+		assert.NotNil(t, instance)
 		err := instance.ResolveContent()
 		assert.NoError(t, err)
+		item, err := instance.GetContentById(content[0].Uri)
+		assert.NoError(t, err)
+		assert.NotNil(t, item)
+		assert.ObjectsAreEqual(item.ContentType, bt.ResourceIdentifier_CID)
 	})
 }
