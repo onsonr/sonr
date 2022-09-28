70df1835feadae6d129ee97429aa46e917db8d01
Oozyx <hendrik.oosenbrug@gmail.com>
2022-09-28 14:47:07 -0400

    Remove test suite changes

--- pkg/motor/registry_test.go
+++ pkg/motor/registry_test.go
@@ -2,36 +2,98 @@ package motor
 
 import (
 	"fmt"
+	"log"
 	"testing"
 
+	"github.com/sonr-io/sonr/pkg/crypto/mpc"
+	"github.com/sonr-io/sonr/third_party/types/common"
 	mt "github.com/sonr-io/sonr/third_party/types/motor/api/v1"
 	"github.com/stretchr/testify/assert"
 )
 
-func (suite *MotorTestSuite) Test_LoginWithKeys() {
-	suite.T().Run("with password and psk", func(t *testing.T) {
-		pskKey := loadKey(fmt.Sprintf("psk%s", suite.motorWithKeys.Address))
+func Test_CreateAccountWithKeyring(t *testing.T) {
+	req := mt.CreateAccountRequest{
+		Password: "password123",
+	}
+
+	m, _ := EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+	_, err := m.CreateAccount(req)
+	assert.NoError(t, err, "wallet generation succeeds")
+
+	b := m.GetBalance()
+	log.Println("balance:", b)
+
+	// Print the address of the wallet
+	log.Println("address:", m.Address)
+}
+
+func Test_CreateAccountWithKeys(t *testing.T) {
+	aesKey := loadKey("aes.key")
+	if aesKey == nil || len(aesKey) != 32 {
+		key, err := mpc.NewAesKey()
+		assert.NoError(t, err, "generates aes key")
+		aesKey = key
+
+		// store the key
+		fmt.Printf("stored key? %v\n", storeKey("aes.key", key))
+	} else {
+		fmt.Println("loaded key")
+	}
+
+	psk, err := mpc.NewAesKey()
+	assert.NoError(t, err, "create psk")
+
+	req := mt.CreateAccountWithKeysRequest{
+		Password:  "password123",
+		AesDscKey: aesKey,
+		AesPskKey: psk,
+	}
+
+	m, _ := EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+	_, err = m.CreateAccountWithKeys(req)
+	assert.NoError(t, err, "wallet generation succeeds")
+
+	b := m.GetBalance()
+	log.Println("balance:", b)
+
+	// Print the address of the wallet
+	log.Println("address:", m.Address)
+
+	// store PSK
+	storeKey(fmt.Sprintf("psk%s", m.Address), psk)
+}
+
+func Test_LoginWithKeys(t *testing.T) {
+	t.Run("with password and psk", func(t *testing.T) {
+		pskKey := loadKey(fmt.Sprintf("psk%s", ADDR))
 		if pskKey == nil || len(pskKey) != 32 {
 			t.Errorf("could not load psk key")
 			return
 		}
 
 		req := mt.LoginWithKeysRequest{
-			Did:       suite.motorWithKeys.Address,
+			Did:       ADDR,
 			Password:  "password123",
 			AesPskKey: pskKey,
 		}
 
-		_, err := suite.motor.LoginWithKeys(req)
+		m, _ := EmptyMotor(&mt.InitializeRequest{
+			DeviceId: "test_device",
+		}, common.DefaultCallback())
+		_, err := m.LoginWithKeys(req)
 		assert.NoError(t, err, "login succeeds")
 
 		if err == nil {
-			fmt.Println("balance: ", suite.motor.GetBalance())
-			fmt.Println("address: ", suite.motor.Address)
+			fmt.Println("balance: ", m.GetBalance())
+			fmt.Println("address: ", m.Address)
 		}
 	})
 
-	suite.T().Run("with DSC and PSK", func(t *testing.T) {
+	t.Run("with DSC and PSK", func(t *testing.T) {
 		aesKey := loadKey("aes.key")
 		fmt.Printf("aes: %x\n", aesKey)
 		if aesKey == nil || len(aesKey) != 32 {
@@ -39,63 +101,68 @@ func (suite *MotorTestSuite) Test_LoginWithKeys() {
 			return
 		}
 
-		pskKey := loadKey(fmt.Sprintf("psk%s", suite.motorWithKeys.Address))
+		pskKey := loadKey(fmt.Sprintf("psk%s", ADDR))
 		if pskKey == nil || len(pskKey) != 32 {
 			t.Errorf("could not load psk key")
 			return
 		}
 
 		req := mt.LoginWithKeysRequest{
-			Did:       suite.motorWithKeys.Address,
+			Did:       ADDR,
 			AesDscKey: aesKey,
 			AesPskKey: pskKey,
 		}
 
-		
-		_, err := suite.motor.LoginWithKeys(req)
+		m, _ := EmptyMotor(&mt.InitializeRequest{
+			DeviceId: "test_device",
+		}, common.DefaultCallback())
+		_, err := m.LoginWithKeys(req)
 		assert.NoError(t, err, "login succeeds")
 
 		if err == nil {
-			fmt.Println("balance: ", suite.motor.GetBalance())
-			fmt.Println("address: ", suite.motor.Address)
+			fmt.Println("balance: ", m.GetBalance())
+			fmt.Println("address: ", m.Address)
 		}
 	})
 }
 
-func (suite *MotorTestSuite) Test_LoginWithKeyring() {
+func Test_LoginWithKeyring(t *testing.T) {
 	req := mt.LoginRequest{
-		Did: suite.motor.Address,
-		Password: "password123",
+		Did: ADDR,
 	}
 
-	fmt.Println("Empty Motor generated")
-	_, err := suite.motor.Login(req)
-	assert.NoError(suite.T(), err, "login succeeds")
+	m, _ := EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+	_, err := m.Login(req)
+	assert.NoError(t, err, "login succeeds")
 
 	if err == nil {
-		fmt.Println("balance: ", suite.motor.GetBalance())
-		fmt.Println("address: ", suite.motor.Address)
+		fmt.Println("balance: ", m.GetBalance())
+		fmt.Println("address: ", m.Address)
 	}
 }
 
-func (suite *MotorTestSuite) Test_LoginAndMakeRequest() {
-	pskKey := loadKey(fmt.Sprintf("psk%s", suite.motorWithKeys.Address))
+func Test_LoginAndMakeRequest(t *testing.T) {
+	pskKey := loadKey(fmt.Sprintf("psk%s", ADDR))
 	if pskKey == nil || len(pskKey) != 32 {
-		suite.T().Errorf("could not load psk key")
+		t.Errorf("could not load psk key")
 		return
 	}
 
-	req := mt.LoginWithKeysRequest{
-		Did:       suite.motorWithKeys.Address,
-		Password:  "password123",
-		AesPskKey: pskKey,
+	req := mt.LoginRequest{
+		Did:      ADDR,
+		Password: "password123",
 	}
 
-	_, err := suite.motorWithKeys.LoginWithKeys(req)
-	assert.NoError(suite.T(), err, "login succeeds")
+	m, _ := EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+	_, err := m.Login(req)
+	assert.NoError(t, err, "login succeeds")
 
 	// do something with the logged in account
-	suite.motorWithKeys.DIDDocument.AddAlias("gotest.snr")
-	_, err = updateWhoIs(suite.motorWithKeys)
-	assert.NoError(suite.T(), err, "updates successfully")
+	m.DIDDocument.AddAlias("gotest.snr")
+	_, err = updateWhoIs(m)
+	assert.NoError(t, err, "updates successfully")
 }
