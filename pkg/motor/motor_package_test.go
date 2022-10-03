146ecc01605e15193ec683db85ed92c8acc56c4b
Oozyx <hendrik.oosenbrug@gmail.com>
2022-10-03 11:39:08 -0500

    Add motor test suite

---
+++ pkg/motor/motor_package_test.go
@@ -0,0 +1,99 @@
+package motor
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sonr-io/sonr/pkg/crypto/mpc"
+	"github.com/sonr-io/sonr/third_party/types/common"
+	mt "github.com/sonr-io/sonr/third_party/types/motor/api/v1"
+	"github.com/stretchr/testify/suite"
+)
+
+type MotorTestSuite struct {
+	suite.Suite
+	motor *motorNodeImpl
+	motorWithKeys *motorNodeImpl
+}
+
+func (suite *MotorTestSuite) SetupSuite() {
+	fmt.Println("Setting up suite...")
+
+	var err error
+
+	// setup motor
+	suite.motor, err = EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+	if err != nil {
+		suite.T().Error("Failed to setup test suite motor")
+	}
+
+	err = setupTestAddress(suite.motor)
+	if err != nil {
+		suite.T().Error("Failed to setup test address")
+	}
+
+	suite.motorWithKeys, err = EmptyMotor(&mt.InitializeRequest{
+		DeviceId: "test_device",
+	}, common.DefaultCallback())
+
+	if err != nil {
+		suite.T().Error("Failed to setup test suite motor with keys")
+	}
+
+	err = setupTestAddressWithKeys(suite.motorWithKeys)
+	if err != nil {
+		suite.T().Error("Failed to setup test address with keys")
+	}
+
+	fmt.Printf("Setup test address: %s\n", suite.motor.Address)
+	fmt.Printf("Setup test address with keys: %s\n", suite.motorWithKeys.Address)
+}
+
+func Test_MotorTestSuite(t *testing.T) {
+	suite.Run(t, new(MotorTestSuite))
+}
+
+func setupTestAddressWithKeys(motor *motorNodeImpl) (error) {
+	aesKey := loadKey("aes.key")
+	if aesKey == nil || len(aesKey) != 32 {
+		key, err := mpc.NewAesKey()
+		if err != nil {
+			return err
+		}
+		aesKey = key
+	}
+
+	psk, err := mpc.NewAesKey()
+	if err != nil {
+		return err
+	}
+
+	req := mt.CreateAccountWithKeysRequest{
+		Password:  "password123",
+		AesDscKey: aesKey,
+		AesPskKey: psk,
+	}
+
+	_, err = motor.CreateAccountWithKeys(req)
+	if err != nil {
+		return err
+	}
+
+	storeKey(fmt.Sprintf("psk%s", motor.Address), psk)
+
+	return nil
+}
+
+func setupTestAddress(motor *motorNodeImpl) (error) {
+	req := mt.CreateAccountRequest{
+		Password: "password123",
+	}
+	_, err := motor.CreateAccount(req)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
