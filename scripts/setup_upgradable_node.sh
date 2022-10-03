59066532661d0ade37059b02297be20717f4802b
uvroxx <uvroxx2323@gmail.com>
2022-10-03 14:17:39 -0400

    refactor

--- scripts/setup_upgradable_node.sh
+++ scripts/setup_upgradable_node.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 export DAEMON_NAME=sonrd
-export SONR_V1_HOME=/home/ut/sonr_cosmvisor/temp/
-export SONR_V2_HOME=/home/ut/sonr_cosmvisor/temp/
-export DAEMON_HOME=/path/to/sonrd-home
+export SONR_V1_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/bin_1
+export SONR_V2_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/bin_2
+export DAEMON_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/daemon
 # Tmp cosmovisor directory. In this directory, we are going to
 # setup Cosmovisor directory structures and copy this directory
 # into $DAEMON_HOME
-export TMP_COSMOVISOR_DIR=/path/to/tmp-cosmovisor
+export TMP_COSMOVISOR_DIR=/home/ut/sonr_cosmvisor/temp/
 # update if needed
 export DAEMON_ALLOW_DOWNLOAD_BINARIES=false
 export DAEMON_RESTART_AFTER_UPGRADE=true
 # setup cored binaries
-cd $SONR_V1_HOME
-make build
-cp ./build/sonrd $TMP_COSMOVISOR_DIR/genesis/bin/
-cd $SONR_V2_HOME
-make build
-cp ./build/sonrd $TMP_COSMOVISOR_DIR/upgrades/v2.0.0/bin/
+ignite chain build -t linux:amd64 --output .
+cp sonrd $SONR_V1_HOME
+cp sonrd $SONR_V2_HOME
 # setup cored data
-cd $SONR_V1_HOME
 ignite chain init --home $DAEMON_HOME
-cp -R $TMP_COSMOVISOR_DIR $DAEMON_HOME/
+#cp -R $TMP_COSMOVISOR_DIR $DAEMON_HOME/
 cosmovisor run start --home $DAEMON_HOME
