c8091b4f04051ad930fbfd892dc2d03f05ac1a3f
utkarsh <utkarsh@webisoft.com>
2022-10-05 13:35:07 -0400

    updated setup script.

--- scripts/setup_upgradable_node.sh
+++ scripts/setup_upgradable_node.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+echo "Setting Up Base Directory"
+echo "Enter Base Folder Name"
+read -r dir
+echo "Enter Sonrd Binary Path (Hint: use whereis sonrd)"
+read -r path
+export DAEMON_HOME="$PWD"/$dir/daemon_home
 export DAEMON_NAME=sonrd
-export SONR_V1_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/bin_1
-export SONR_V2_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/bin_2
-export DAEMON_HOME=/home/ut/sonr_cosmvisor/comovisor_temp/daemon
-# Tmp cosmovisor directory. In this directory, we are going to
-# setup Cosmovisor directory structures and copy this directory
-# into $DAEMON_HOME
-export TMP_COSMOVISOR_DIR=/home/ut/sonr_cosmvisor/temp/
-# update if needed
 export DAEMON_ALLOW_DOWNLOAD_BINARIES=false
 export DAEMON_RESTART_AFTER_UPGRADE=true
-# setup cored binaries
-ignite chain build -t linux:amd64 --output .
-cp sonrd $SONR_V1_HOME
-cp sonrd $SONR_V2_HOME
-# setup cored data
-ignite chain init --home $DAEMON_HOME
-#cp -R $TMP_COSMOVISOR_DIR $DAEMON_HOME/
-cosmovisor run start --home $DAEMON_HOME
+mkdir "$dir"
+# shellcheck disable=SC2164
+cd "$dir"
+mkdir daemon_home
+mkdir daemon_home/data
+mkdir daemon_home/config
+# shellcheck disable=SC2164
+cd daemon_home
+# Installing in case its not installed
+go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@latest
+cosmovisor init $path
+echo "Done"
