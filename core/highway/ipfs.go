a940bf5b6bcb80102af350305aab281dc46d83f4
Prad Nukala <pradn@sonr.io>
2023-01-07 17:07:03 -0500

    Update IPFS Methods to allow custom gateway

--- core/highway/ipfs.go
+++ core/highway/ipfs.go
@@ -2,6 +2,9 @@ package highway
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/sonr-hq/sonr/pkg/ipfs"
@@ -35,33 +38,80 @@ func (v *IPFSService) Add(ctx context.Context, req *v1.AddRequest) (*v1.AddRespo
 		return nil, err
 	}
 	return &v1.AddResponse{
-		Hash: cid,
+		Hash:          cid,
+		ContentLength: int64(len(req.GetContent())),
 	}, nil
 }
 
 // Exists checks if the given key exists in the keyring.
 func (v *IPFSService) Exists(ctx context.Context, req *v1.ExistsRequest) (*v1.ExistsResponse, error) {
-	content, err := v.highway.Get(req.GetHash())
-	if err != nil {
-		return nil, err
-	}
-	if content == nil {
+	// Check if gateway was provided.
+	if req.GetGateway() != "" {
+		// Perform a GET request to the gateway.
+		resp, err := http.Get(fmt.Sprintf("%s/ipfs/%s", req.GetGateway(), req.GetHash()))
+		if err != nil {
+			return nil, err
+		}
+		// If the response is 200, then the key exists.
+		if resp.StatusCode == http.StatusOK {
+			return &v1.ExistsResponse{
+				Exists: true,
+			}, nil
+		}
+		// Otherwise, the key does not exist.
 		return &v1.ExistsResponse{
 			Exists: false,
 		}, nil
 	}
+	// Fetch the content from the IPFS node.
+	content, err := v.highway.Get(req.GetHash())
+	if err != nil {
+		return nil, err
+	}
+
+	// If the content is nil, then the key does not exist.
+	if content == nil {
+		return &v1.ExistsResponse{
+			Exists: false,
+			Hash:   req.GetHash(),
+		}, nil
+	}
+
+	// Otherwise, the key exists.
 	return &v1.ExistsResponse{
 		Exists: true,
+		Hash:   req.GetHash(),
 	}, nil
 }
 
 // Refresh refreshes the keypair and returns the public key.
 func (v *IPFSService) Get(ctx context.Context, req *v1.GetRequest) (*v1.GetResponse, error) {
+	// Check if gateway was provided.
+	if req.GetGateway() != "" {
+		// Perform a GET request to the gateway.
+		resp, err := http.Get(fmt.Sprintf("%s/ipfs/%s", req.GetGateway(), req.GetHash()))
+		if err != nil {
+			return nil, err
+		}
+
+		// If the response is 200, then the key exists.
+		if resp.StatusCode == http.StatusOK {
+			bz, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return nil, err
+			}
+			return &v1.GetResponse{
+				Content: bz,
+				Hash:    req.GetHash(),
+			}, nil
+		}
+	}
 	bz, err := v.highway.Get(req.GetHash())
 	if err != nil {
 		return nil, err
 	}
 	return &v1.GetResponse{
 		Content: bz,
+		Hash:    req.GetHash(),
 	}, nil
 }
