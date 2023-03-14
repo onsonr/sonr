acd212cd535693a5bda339953679345f27cbc143
Prad Nukala <pradn@sonr.io>
2023-03-14 18:21:01 -0400

    Add query method which adds accs on chain for pubkeys

--- ts-client/cosmos.mint.v1beta1/types.ts
+++
@@ -1,9 +0,0 @@
-import { Minter } from "./types/cosmos/mint/v1beta1/mint"
-import { Params } from "./types/cosmos/mint/v1beta1/mint"
-
-
-export {     
-    Minter,
-    Params,
-    
- }
--- ts-client/ibc.applications.interchain_accounts.controller.v1/types.ts
+++
@@ -1,7 +0,0 @@
-import { Params } from "./types/ibc/applications/interchain_accounts/controller/v1/controller"
-
-
-export {     
-    Params,
-    
- }
