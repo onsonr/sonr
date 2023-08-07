622a9ee412d2aa175618186c8e0b4d49eea2e110
Prad Nukala <prad@sonr.io>
2023-08-06 20:58:39 -0400

    * chore(Taskfile.yml): add --no-cache flag to docker build command for building sonrd image * chore(Taskfile.yml): add --no-cache flag to docker build command for building sonr-faucet image
    * fix(context.go): replace panic with print statement when no config file is found
    * refactor(context.go): use environment variables only when no config file is found
    
    * chore(context.go): remove unnecessary whitespace changes
    * refactor(context.go): simplify variable assignments in Get() function

--- config/context.go
+++ config/context.go
@@ -18,11 +18,11 @@ func init() {
 	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err != nil {
-		panic(err)
+		fmt.Println("No config file found - using environment variables only")
 	}
 	err = viper.Unmarshal(&c)
 	if err != nil {
-		panic(err)
+		fmt.Println("No config file found - using environment variables only")
 	}
 }
 
@@ -33,38 +33,38 @@ func Get() Config {
 
 var (
 	// ChainID returns the chain ID from the environment variable SONR_CHAIN_ID. (default: sonr-localnet-1)
-	ChainID               = c.ChainID
+	ChainID = c.ChainID
 
 	// HighwayHostPort returns the host and port of the Highway API
-	HighwayHostPort       = fmt.Sprintf("%s:%d", c.Launch.Highway.API.Host, c.Launch.Highway.API.Port)
+	HighwayHostPort = fmt.Sprintf("%s:%d", c.Launch.Highway.API.Host, c.Launch.Highway.API.Port)
 
 	// HighwayRequestTimeout returns the timeout for Highway API requests
 	HighwayRequestTimeout = time.Duration(c.Launch.Highway.API.Timeout) * time.Second
 
 	// IceFireHost returns the host and port of the IceFire KV store
-	IceFireHost           = fmt.Sprintf("%s:%d", c.Launch.Highway.DB.IcefireKV.Host, c.Launch.Highway.DB.IcefireKV.Port)
+	IceFireHost = fmt.Sprintf("%s:%d", c.Launch.Highway.DB.IcefireKV.Host, c.Launch.Highway.DB.IcefireKV.Port)
 
 	// JWTSigningKey returns the JWT signing key
-	JWTSigningKey         = []byte(c.Launch.Highway.SigningKey)
+	JWTSigningKey = []byte(c.Launch.Highway.SigningKey)
 
 	// NodeAPIHost returns the host and port of the Node API
-	NodeAPIHost           = fmt.Sprintf("%s:%d", c.Launch.Node.API.Host, c.Launch.Node.API.Port)
+	NodeAPIHost = fmt.Sprintf("%s:%d", c.Launch.Node.API.Host, c.Launch.Node.API.Port)
 
 	// NodeGrpcHost returns the host and port of the Node gRPC
-	NodeGrpcHost          = fmt.Sprintf("%s:%d", c.Launch.Node.GRPC.Host, c.Launch.Node.GRPC.Port)
+	NodeGrpcHost = fmt.Sprintf("%s:%d", c.Launch.Node.GRPC.Host, c.Launch.Node.GRPC.Port)
 
 	// NodeP2PHost returns the host and port of the Node P2P
-	NodeP2PHost           = fmt.Sprintf("%s:%d", c.Launch.Node.P2P.Host, c.Launch.Node.P2P.Port)
+	NodeP2PHost = fmt.Sprintf("%s:%d", c.Launch.Node.P2P.Host, c.Launch.Node.P2P.Port)
 
 	// NodeRPCHost returns the host and port of the Node RPC
-	NodeRPCHost           = fmt.Sprintf("%s:%d", c.Launch.Node.RPC.Host, c.Launch.Node.RPC.Port)
+	NodeRPCHost = fmt.Sprintf("%s:%d", c.Launch.Node.RPC.Host, c.Launch.Node.RPC.Port)
 
 	// Environment returns the environment from the environment variable SONR_ENVIRONMENT. (default: development)
-	Environment           = c.Environment
+	Environment = c.Environment
 
 	// IsProduction returns true if the environment is production
-	IsProduction          = c.Environment == "production"
+	IsProduction = c.Environment == "production"
 
 	// ValidatorAddress returns the validator address from the environment variable SONR_VALIDATOR_ADDRESS.
-	ValidatorAddress      = c.Launch.Address
+	ValidatorAddress = c.Launch.Address
 )
