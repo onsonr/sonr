d2dd51465f226afca0fb1d982183e811cf2ffba7
Prad Nukala <prad@sonr.io>
2023-08-28 14:32:26 -0400

    * fix(constants.js): update main_addr constant value to "idx1vl4xg8quydtgj9p7psqcpzs9x5azv534hdh49q"

--- testutil/wasm/constants.js
+++ testutil/wasm/constants.js
@@ -1,2 +1,2 @@
-export const main_addr = "snr1vl4xg8quydtgj9p7psqcpzs9x5azv534hdh49q"
+export const main_addr = "idx1vl4xg8quydtgj9p7psqcpzs9x5azv534hdh49q"
 export const NftContractAddress = "artifacts/cw721_base.wasm"
