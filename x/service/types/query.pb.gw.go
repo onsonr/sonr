b71f3bb91a1c0b3e3bf8408c292312b9303dab59
Prad Nukala <prad@sonr.io>
2023-09-03 14:01:33 -0400

    Update type defs

--- x/service/types/query.pb.gw.go
+++ x/service/types/query.pb.gw.go
@@ -321,11 +321,11 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"core", "params", "service"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"core", "params", "service"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ServiceRecord_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"core", "service", "origin"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_ServiceRecord_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"core", "service", "origin"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ServiceRecordAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"core", "service"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_ServiceRecordAll_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"core", "service"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
