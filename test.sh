f14a01df40ee98a71f1fdecb99574e3b35f364bd
Prad Nukala <prad@sonr.io>
2023-11-28 10:11:03 -0500

    fix: remove empty script

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -fsS https://pkgx.sh | sh
-
-pkgx +gh +gum +docker +git +make +bash which node  #=> /tmp/pkgx.sh/nodejs.org/v16/bin/node
-
-gum confirm "Are you sure you want to deploy?" && gh release create v1.0.0
