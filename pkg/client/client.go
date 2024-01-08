1e4c7d6bcfc679d559f9e8841de5820f41f393f7
Prad Nukala <prad@sonr.io>
2024-01-08 07:10:19 +0000

    Jan 8, 2024, 2:10 AM

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,5 +1,12 @@
 package client
 
-type BlockchainClient struct {
+import "github.com/sonrhq/sonr/pkg/client/modules"
 
+type BlockchainClient struct {
+    Bank modules.BankClient
+    Gov modules.GovClient
+    Identity modules.IdentityClient
+    Service modules.ServiceClient
+    Staking modules.StakingClient
+    Tendermint modules.TendermintClient
 }
