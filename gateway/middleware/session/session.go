5c770ea975011750af99e1f18518b796a8648a7d
Prad Nukala <prad@sonr.io>
2024-01-09 16:20:11 -0500

    Remove session middleware and unused imports

--- gateway/middleware/session/session.go
+++ gateway/middleware/session/session.go
@@ -1,4 +1,4 @@
-package middleware
+package session
 
 import (
 	"net/http"
--- gateway/router.go
+++ gateway/router.go
@@ -8,8 +8,6 @@ import (
 	// "github.com/alexandrevicenzi/go-sse"
 	"github.com/go-chi/chi/v5"
 	chimdw "github.com/go-chi/chi/v5/middleware"
-	"github.com/sonrhq/identity"
-	"github.com/sonrhq/service"
 
 	"github.com/sonrhq/sonr/gateway/handlers/home"
 	"github.com/sonrhq/sonr/gateway/handlers/search"
@@ -24,7 +22,5 @@ func Start() {
 	r.Use(chimdw.Recoverer)
     r.Mount("/", home.Routes())
 	r.Mount("/search", search.Routes())
-	identity.RegisterGateway(r)
-	service.RegisterGateway(r)
 	http.ListenAndServe(":8080", r)
 }
