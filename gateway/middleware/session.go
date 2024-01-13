bd54445442ee87fbefe100b634721a6aab7c355d
Prad Nukala <prad@sonr.io>
2024-01-12 16:26:32 -0800

    Refactor session middleware and remove commented code

--- gateway/middleware/session.go
+++ gateway/middleware/session.go
@@ -10,13 +10,12 @@ import (
 
 // UseDefaults adds chi provided middleware libraries to the router.
 func UseDefaults(r *chi.Mux) {
-    r.Use(chimdw.Compress(10))
+	r.Use(chimdw.Compress(10))
 	r.Use(chimdw.RequestID)
 	r.Use(chimdw.RealIP)
 	r.Use(chimdw.Logger)
 	r.Use(chimdw.Recoverer)
 }
-
 
 func Session(next http.Handler) http.Handler {
 	mw := SessionMiddleware{
@@ -42,26 +41,25 @@ func ID(r *http.Request) (id string) {
 }
 
 func (mw SessionMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	// s := sse.NewServer(nil)
 	// defer s.Shutdown()
-    // r.Mount("/events/", s)
+	// r.Mount("/events/", s)
 
-    //     go func() {
-    //         for {
-    //             s.SendMessage("/events/channel-1", sse.SimpleMessage(time.Now().Format("2006/02/01/ 15:04:05")))
-    //             time.Sleep(1 * time.Second)
-    //         }
-    //     }()
+	//     go func() {
+	//         for {
+	//             s.SendMessage("/events/channel-1", sse.SimpleMessage(time.Now().Format("2006/02/01/ 15:04:05")))
+	//             time.Sleep(1 * time.Second)
+	//         }
+	//     }()
 
-    //     go func() {
-    //         i := 0
-    //         for {
-    //             i++
-    //             s.SendMessage("/events/channel-2", sse.SimpleMessage(strconv.Itoa(i)))
-    //             time.Sleep(1 * time.Second)
-    //         }
-    //     }()
+	//     go func() {
+	//         i := 0
+	//         for {
+	//             i++
+	//             s.SendMessage("/events/channel-2", sse.SimpleMessage(strconv.Itoa(i)))
+	//             time.Sleep(1 * time.Second)
+	//         }
+	//     }()
 
 	id := ID(r)
 	if id == "" {
