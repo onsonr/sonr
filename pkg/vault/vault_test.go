601bcf017545f4288a8112fc820e276d243c00e3
Prad Nukala <prad@sonr.io>
2024-01-18 12:52:01 -0800

    fix: test suite check for ipfs conn

--- pkg/vault/vault_test.go
+++ pkg/vault/vault_test.go
@@ -4,14 +4,24 @@ import (
 	"context"
 	"testing"
 
-	"github.com/sonrhq/sonr/pkg/vault"
+	"github.com/ipfs/kubo/client/rpc"
 	"github.com/stretchr/testify/require"
+
+	"github.com/sonrhq/sonr/pkg/vault"
 )
 
 func TestNewVault(t *testing.T) {
+	if !checkLocalIPFSConn() {
+		t.Skip("Skipping test due to no local IPFS connection")
+	}
 	v, err := vault.New(context.Background())
 	require.NoError(t, err)
 	require.NotEmpty(t, v.Key)
 	t.Logf("PeerID: %s", v.PeerID)
 	t.Logf("SonrAddress: %s", v.SonrAddress)
+}
+
+func checkLocalIPFSConn() bool {
+	_, err := rpc.NewLocalApi()
+	return err == nil
 }
