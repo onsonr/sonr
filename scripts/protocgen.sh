c6b4fce47a214b81667c8a31c4982a075f141a0d
Prad Nukala <prad@sonr.io>
2024-01-19 20:14:50 -0800

    init: update existing gogo generated types

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -3,14 +3,14 @@
 set -e
 
 
-cd proto/sonrhq
+cd proto
 echo "Generating gogo proto code"
-proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-      buf generate --template buf.gen.gogo.yaml $file
-  done
-done
-
+# proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+# for dir in $proto_dirs; do
+#   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+#       buf generate --template buf.gen.gogo.yaml $file
+#   done
+# done
+buf generate --template buf.gen.gogo.yaml
 buf generate --template buf.gen.pulsar.yaml
 
