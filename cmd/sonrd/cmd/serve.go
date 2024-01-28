b923b75eae376358793e0720d857768f44586af9
Prad Nukala <prad@sonr.io>
2024-01-27 17:30:57 -0800

    fix: cmd banner port for highway

--- cmd/sonrd/cmd/serve.go
+++ cmd/sonrd/cmd/serve.go
@@ -8,30 +8,28 @@ import (
 	"github.com/sonrhq/sonr/app/highway"
 )
 
+const persistentHeader = `
+Sonr Highway
+· Gateway: http://localhost:8000
+· Node RPC: http://localhost:26657
+`
+
+// ServeCommand returns the serve command
 func ServeCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:                        "serve",
 		Short:                      "Serves the Sonr Highway",
 		DisableFlagParsing:         false,
 		SuggestionsMinimumDistance: 2,
-		Run:                        serveGateway,
+		Run:                        serveAction,
 	}
 	return cmd
 }
 
-func serveGateway(_ *cobra.Command, _ []string) {
+func serveAction(_ *cobra.Command, _ []string) {
 	// 1. Read config from file
-
 	// 2. Check reachable to enabled services
-
 	// 3. Start Gateway router as system service
-
 	pterm.DefaultHeader.Printf(persistentHeader)
 	highway.Start()
 }
-
-const persistentHeader = `
-Sonr Highway
-· Gateway: http://localhost:8080
-· Node RPC: http://localhost:26657
-`
