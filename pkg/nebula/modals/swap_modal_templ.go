40cd6644ea06c20dd68617391152ab57eb1a6063
Prad Nukala <prad@sonr.io>
2024-01-27 17:17:25 -0800

    Update dialog widths in swap and share modals

--- pkg/nebula/modals/swap_modal_templ.go
+++ pkg/nebula/modals/swap_modal_templ.go
@@ -23,7 +23,7 @@ func SwapTokensModal() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<sl-dialog label=\"Dialog\" class=\"dialog-width\" style=\"--width: 50vw;\" open>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<sl-dialog label=\"Dialog\" class=\"dialog-width\" style=\"--width: 40vw;\" open>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
