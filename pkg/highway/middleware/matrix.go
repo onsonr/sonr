341957352e52331fc4b41b875d2c1ae35671c85e
Prad Nukala <prad@sonr.io>
2024-02-15 15:11:37 -0800

    feat(nitro): add client functions for registering a new user and creating a room in the Synapse server
    feat(highway): add Matrix middleware struct and constructor function to handle Matrix integration
    
    refactor(Dev.yml): remove unnecessary directory specification for the highway task

--- pkg/highway/middleware/matrix.go
+++ pkg/highway/middleware/matrix.go
@@ -1,1 +1,13 @@
 package middleware
+
+type Matrix struct {
+	HomeServer   string
+	sharedSecret string
+}
+
+func NewMatrix(homeServer, sharedSecret string) *Matrix {
+	return &Matrix{
+		HomeServer:   homeServer,
+		sharedSecret: sharedSecret,
+	}
+}
