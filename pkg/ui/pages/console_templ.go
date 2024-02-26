d0ba38e40ad7ce757f0f20619d3e73b2cc58fdaa
Prad Nukala <prad@sonr.io>
2024-02-26 16:09:55 -0500

    fix: update query service method for retreiving webauthn credentials

--- pkg/ui/pages/console_templ.go
+++ pkg/ui/pages/console_templ.go
@@ -83,7 +83,7 @@ func chatPanel() templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = views.ConversationsList().Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = components.ConversationsList().Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
