77ba6114fdd3d78beb34c1bd460344bf8beaf70c
Prad Nukala <prad@sonr.io>
2024-03-06 15:27:31 -0500

    func (p pages) Root(c echo.Context) error {     if common.JWT(c).HasController() {         return common.Render(c, chats.Page(c))     }     if common.Cookies(c).HasHandle() {         return common.Render(c, login.Page(c))     }     if common.Requests(c).PathIs("/register") {         return common.Render(c, register.Page(c))     }     return common.Render(c, home.Page(c)) }
    func (p pages) Login(c echo.Context) error {
        return common.Render(c, login.Page(c))
    }
    
    func (p pages) Register(c echo.Context) error {
        return common.Render(c, register.Page(c))
    }
    
    func (p pages) Home(c echo.Context) error {
        return common.Render(c, home.Page(c))
    }
    
    func (p pages) Chats(c echo.Context) error {
        return common.Render(c, chats.Page(c))
    }
    
    func (p pages) Changelog(c echo.Context) error {
        return common.Render(c, changelog.Page(c))
    }
    
    func (p pages) Error(c echo.Context) error {
        return common.Render(c, utility.ErrorNotFound(c))
    }
    
    func (p pages) NotFound(c echo.Context) error {
        return common.Render(c, utility.ErrorNotFound(c))
    }
    
    func (p pages) Ecosystem(c echo.Context) error {
        return common.Render(c, ecosystem.Page(c))
    }
    
    func (p pages) About(c echo.Context) error {
        return common.Render(c, about.Page(c))
    }
    
    func (p pages) Research(c echo.Context) error {
        return common.Render(c, research.Page(c))
    }

--- pkg/handlers/api/identity_handler.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-// IdentityAPI is a handler for the staking module
-var IdentityAPI = identityAPI{}
-
-// identityAPI is a handler for the staking module
-type identityAPI struct{}
