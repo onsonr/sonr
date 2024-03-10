95d5c9623f494543c5697fefd8446cfd2bcbcebc
Prad Nukala <prad@sonr.io>
2024-03-09 23:38:56 -0600

    chore(cmds.go): remove unused code and dependencies from cmds.go file feat(main.go): add version command to print the version number of Sonr Highway feat(main.go): refactor main function to use cobra for command line interface and improve code organization

--- cmd/hway/main.go
+++ cmd/hway/main.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/pterm/pterm"
+	"github.com/sonrhq/sonr/cmd/hway/config"
+	"github.com/sonrhq/sonr/pkg/middleware/common"
+	"github.com/sonrhq/sonr/pkg/routes"
+	"github.com/spf13/cobra"
 )
 
 var (
@@ -22,6 +31,40 @@ func init() {
 
 // main is the entry point for the application.
 func main() {
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of Sonr Highway",
+		Run: func(cmd *cobra.Command, args []string) {
+			pterm.Info.Println(fmt.Sprintf("Version: %s", Version))
+		},
+	}
+
+	rootCmd := &cobra.Command{
+		Use:                        "hway",
+		Short:                      "Serves the Sonr Highway",
+		DisableFlagParsing:         false,
+		SuggestionsMinimumDistance: 2,
+		Run: func(c *cobra.Command, _ []string) {
+			// Create the config
+			cnfg := config.NewHway()
+			cnfg.ReadFlags(c)
+
+			// Create the router
+			e := echo.New()
+			e.Logger.SetLevel(log.INFO)
+			common.UseDefaults(e)
+
+			// Register the routes
+			routes.RegisterCosmosAPI(e)
+			routes.RegisterSonrAPI(e)
+			routes.RegisterUI(e)
+
+			// Serve the router
+			cnfg.PrintBanner()
+			e.Logger.Fatal(e.Start(cnfg.ListenAddress()))
+		},
+	}
+
 	rootCmd.Flags().String("hway-host", "0.0.0.0", "host")
 	rootCmd.Flags().Int("hway-port", 8000, "port")
 	rootCmd.Flags().String("hway-psql", "postgresql://sonr:sonr@localhost:5432/sonr?sslmode=disable", "psql connection string")
