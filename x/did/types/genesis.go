9d48ba585045f84a7a325308e1ff44b79d095b3c
Prad Nukala <prad@sonr.io>
2024-06-29 19:37:34 -0400

    Update Params

--- x/did/types/genesis.go
+++ x/did/types/genesis.go
@@ -11,8 +11,7 @@ const DefaultIndex uint64 = 1
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
 		// this line is used by starport scaffolding # genesis/types/default
-		Params:      DefaultParams(),
-		Controllers: make([]Controller, 0),
+		Params: DefaultParams(),
 	}
 }
 
