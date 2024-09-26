467d1c9b27ad39afc41746246119c1cf405b2488
Prad Nukala <prad@sonr.io>
2024-09-26 13:05:09 -0400

    refactor: rename  script to

--- nebula/nebula.go
+++ nebula/nebula.go
@@ -11,6 +11,7 @@ import (
 //go:embed assets
 var embeddedFiles embed.FS
 
+// UseAssets is a middleware that serves static files from the embedded assets
 func UseAssets(e *echo.Echo) echo.HandlerFunc {
 	embFs := http.FS(os.DirFS("assets"))
 	assets := http.FileServer(embFs)
