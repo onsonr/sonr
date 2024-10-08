b5dc577d018f8f5882304179a3f605fd589a73da
Prad Nukala <prad@sonr.io>
2024-10-07 23:16:03 -0400

    refactor: rename 'global/cdn' to 'assets'

--- pkg/nebula/global/styles/layout_templ.go
+++ pkg/nebula/global/styles/layout_templ.go
@@ -9,7 +9,7 @@ import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
 import (
-	"github.com/onsonr/sonr/pkg/nebula/global/cdn"
+	cdn "github.com/onsonr/sonr/pkg/nebula/assets"
 	"strings"
 )
 
