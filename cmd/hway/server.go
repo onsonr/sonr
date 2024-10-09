ea37de8349bcb6232a21d52b557c6b712bc9f433
Prad Nukala <prad@sonr.io>
2024-10-09 16:20:04 -0400

    refactor: abstract template rendering to ctx module

--- cmd/hway/server.go
+++ cmd/hway/server.go
@@ -5,15 +5,16 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/onsonr/sonr/internal/ctx"
-	"github.com/onsonr/sonr/pkg/nebula/routes"
+	"github.com/onsonr/sonr/pkg/nebula/components/auth"
+	"github.com/onsonr/sonr/pkg/nebula/components/home"
 	"github.com/syumai/workers"
 )
 
 func main() {
 	s := echo.New()
 	s.Use(ctx.UseSession)
-	s.GET("/", routes.Home)
-	s.GET("/login", routes.LoginStart)
-	s.GET("/register", routes.RegisterStart)
+	s.GET("/", home.Route)
+	s.GET("/login", auth.LoginRoute)
+	s.GET("/register", auth.RegisterRoute)
 	workers.Serve(s)
 }
--- pkg/dwn/embed.go
+++ pkg/dwn/embed.go
@@ -1,14 +1,11 @@
 package dwn
 
 import (
-	"bytes"
-	"context"
 	_ "embed"
-	"encoding/json"
 
 	"github.com/ipfs/boxo/files"
 	"github.com/onsonr/sonr/pkg/dwn/gen"
-	"github.com/onsonr/sonr/pkg/nebula/routes"
+	"github.com/onsonr/sonr/pkg/nebula/components/index"
 )
 
 //go:embed app.wasm
@@ -24,41 +21,14 @@ var (
 
 // NewVaultDirectory creates a new directory with the default files
 func NewVaultDirectory(cnfg *gen.Config) (files.Node, error) {
-	dwnJSON, err := json.Marshal(cnfg)
-	if err != nil {
-		return nil, err
-	}
-
-	w := bytes.NewBuffer(nil)
-	err = routes.IndexFile().Render(context.Background(), w)
+	idxFile, err := index.BuildFile(cnfg)
 	if err != nil {
 		return nil, err
 	}
 	fileMap := map[string]files.Node{
-		"config.json": files.NewBytesFile(dwnJSON),
-		"sw.js":       swJSFile,
-		"app.wasm":    dwnWasmFile,
-		"index.html":  files.NewBytesFile(w.Bytes()),
+		"sw.js":      swJSFile,
+		"app.wasm":   dwnWasmFile,
+		"index.html": idxFile,
 	}
 	return files.NewMapDirectory(fileMap), nil
-}
-
-// Use IndexHTML template to generate the index file
-func IndexHTMLFile() (files.Node, error) {
-	w := bytes.NewBuffer(nil)
-	err := routes.IndexFile().Render(context.Background(), w)
-	if err != nil {
-		return nil, err
-	}
-	indexData := w.Bytes()
-	return files.NewBytesFile(indexData), nil
-}
-
-// MarshalConfigFile uses the config template to generate the dwn config file
-func MarshalConfigFile(c *gen.Config) (files.Node, error) {
-	dwnConfigData, err := json.Marshal(c)
-	if err != nil {
-		return nil, err
-	}
-	return files.NewBytesFile(dwnConfigData), nil
 }
