2df1e1559b0988f36bb9c08b62615cde001a5e6a
Prad Nukala <prad@sonr.io>
2024-10-10 12:58:22 -0400

    feat: remove unused configuration files

--- scripts/init_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-export ACC0_MNEMONIC=$(skate get ACC0_MNEMONIC)
-export ACC1_MNEMONIC=$(skate get ACC1_MNEMONIC)
-export CHAIN_ID=$(skate get CHAIN_ID)
-export DENOM=$(skate get DENOM)
-export KEYRING=$(skate get KEYRING)
-export MONIKER=$(skate get MONIKER)
