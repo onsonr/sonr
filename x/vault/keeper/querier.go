75d2de9c4204aeb7b8a40bca33779fce736e4bec
Prad Nukala <prad@sonr.io>
2024-10-13 16:20:53 -0400

    fix(keeper): Handle missing controller during initial sync

--- x/vault/keeper/querier.go
+++ x/vault/keeper/querier.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"context"
-	"errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -50,12 +49,14 @@ func (k Querier) SyncInitial(goCtx context.Context, req *types.SyncInitialReques
 	if err != nil {
 		return nil, err
 	}
-
 	c, _ := k.DIDKeeper.ResolveController(ctx, req.Did)
 	if c == nil {
-		return nil, errors.New("controller not found")
+		return &types.SyncInitialResponse{
+			Success: false,
+			Schema:  p.Schema,
+			ChainID: ctx.ChainID(),
+		}, nil
 	}
-
 	return &types.SyncInitialResponse{
 		Success: true,
 		Schema:  p.Schema,
