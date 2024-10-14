36139555d984b29bed403a3a4e9e3f11d00ba077
Prad Nukala <prad@sonr.io>
2024-10-14 18:53:07 -0400

    feat(gen): Remove generated code for IPFS, Motr and Sonr

--- internal/dwn/gen/IPFS.pkl.go
+++
@@ -1,8 +0,0 @@
-// Code generated from Pkl module `dwngen`. DO NOT EDIT.
-package gen
-
-type IPFS struct {
-	ApiUrl string `pkl:"apiUrl" json:"apiUrl,omitempty"`
-
-	GatewayUrl string `pkl:"gatewayUrl" json:"gatewayUrl,omitempty"`
-}
