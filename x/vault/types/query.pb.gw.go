c1b4005d7f1015d922b66ef9abf4e5f1fd754b05
Prad Nukala <prad@sonr.io>
2024-10-18 12:25:32 -0400

    refactor: rename SyncCurrent to Sync

--- x/vault/types/query.pb.gw.go
+++ x/vault/types/query.pb.gw.go
@@ -491,7 +491,7 @@ var (
 
 	pattern_Query_SyncInitial_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vault", "v1", "sync-initial"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_SyncCurrent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vault", "v1", "sync-current"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_SyncCurrent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"vault", "v1", "sync"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
