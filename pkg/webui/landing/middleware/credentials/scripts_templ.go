06330c0b4746e353b1156f30139108b4b09d8fed
Prad Nukala <prad@sonr.io>
2024-12-02 17:57:00 -0500

    feat: add landing page components and middleware for credentials and payments

--- pkg/webui/landing/middleware/credentials/scripts_templ.go
+++ pkg/webui/landing/middleware/credentials/scripts_templ.go
@@ -10,31 +10,6 @@ import templruntime "github.com/a-h/templ/runtime"
 
 var credentialsHandle = templ.NewOnceHandle()
 
-// Types for credential data
-type PublicKeyCredentialCreationOptions struct {
-	Challenge       string `json:"challenge"`
-	RpName          string `json:"rpName"`
-	RpID            string `json:"rpId"`
-	UserID          string `json:"userId"`
-	UserName        string `json:"userName"`
-	UserDisplayName string `json:"userDisplayName"`
-	Timeout         int    `json:"timeout,omitempty"`
-	AttestationType string `json:"attestationType,omitempty"`
-}
-
-type PublicKeyCredentialRequestOptions struct {
-	Challenge        string                 `json:"challenge"`
-	RpID             string                 `json:"rpId"`
-	Timeout          int                    `json:"timeout,omitempty"`
-	UserVerification string                 `json:"userVerification,omitempty"`
-	AllowCredentials []CredentialDescriptor `json:"allowCredentials,omitempty"`
-}
-
-type CredentialDescriptor struct {
-	Type string `json:"type"`
-	ID   string `json:"id"`
-}
-
 // Base credentials script template
 func CredentialsScripts() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
