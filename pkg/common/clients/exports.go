7b03c1978c63c3f24664c89bafa96b7442e59893
Prad Nukala <prad@sonr.io>
2024-12-02 19:16:16 -0500

    refactor: rename client files and improve code structure

---
+++ pkg/common/clients/exports.go
@@ -0,0 +1,7 @@
+package clients
+
+// QueryClient is the interface which wraps around all the query clients
+type QueryClient interface{}
+
+// ServiceClient is the interface which wraps around all the service clients
+type ServiceClient interface{}
