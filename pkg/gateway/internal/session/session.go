6dd81489df8349d2d506d65054b7756ed2b6822a
Prad Nukala <prad@sonr.io>
2024-12-06 00:36:15 -0500

    feat: Add SetUserHandle function to set user handle in session

--- pkg/gateway/internal/session/session.go
+++ pkg/gateway/internal/session/session.go
@@ -1,1 +1,12 @@
 package session
+
+import "github.com/labstack/echo/v4"
+
+// SetUserHandle sets the user handle in the session
+func SetUserHandle(c echo.Context, handle string) error {
+	sess, err := Get(c)
+	if err != nil {
+		return err
+	}
+	sess.Session().UserHandle = handle
+}
