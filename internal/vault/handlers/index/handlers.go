518109e9df7d2355803aba4712c965cc8003d415
Prad Nukala <prad@sonr.io>
2024-12-10 13:33:01 -0500

    refactor: remove unused UI components related to wallet and index pages

--- internal/vault/handlers/index/handlers.go
+++ internal/vault/handlers/index/handlers.go
@@ -4,17 +4,15 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/onsonr/sonr/internal/vault/pages/index"
 	session "github.com/onsonr/sonr/internal/vault/session"
-	"github.com/onsonr/sonr/pkg/common/response"
 )
 
 func HandleIndex(c echo.Context) error {
 	if isInitial(c) {
-		return response.TemplEcho(c, index.InitialView())
+		// return response.TemplEcho(c, index.InitialView())
 	}
 	if isExpired(c) {
-		return response.TemplEcho(c, index.ReturningView())
+		// return response.TemplEcho(c, index.ReturningView())
 	}
 	return c.Render(http.StatusOK, "index.templ", nil)
 }
