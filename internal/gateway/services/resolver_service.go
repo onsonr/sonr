8308dd5c867c62f2d5f4211bc4bc6a634d4a3e9c
Prad Nukala <prad@sonr.io>
2024-12-12 13:42:37 -0500

    feat: add gRPC clients for bank, DID, DWN, and SVC modules

--- internal/gateway/services/resolver_service.go
+++ internal/gateway/services/resolver_service.go
@@ -1,6 +1,12 @@
 package services
 
-import "google.golang.org/grpc"
+import (
+	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
+	didv1 "github.com/onsonr/sonr/api/did/v1"
+	dwnv1 "github.com/onsonr/sonr/api/dwn/v1"
+	svcv1 "github.com/onsonr/sonr/api/svc/v1"
+	"google.golang.org/grpc"
+)
 
 type ResolverService struct {
 	grpcAddr string
@@ -12,4 +18,36 @@ func (s *ResolverService) getClientConn() (*grpc.ClientConn, error) {
 		return nil, err
 	}
 	return grpcConn, nil
+}
+
+func (s *ResolverService) BankQuery() (bankv1beta1.QueryClient, error) {
+	conn, err := s.getClientConn()
+	if err != nil {
+		return nil, err
+	}
+	return bankv1beta1.NewQueryClient(conn), nil
+}
+
+func (s *ResolverService) DIDQuery() (didv1.QueryClient, error) {
+	conn, err := s.getClientConn()
+	if err != nil {
+		return nil, err
+	}
+	return didv1.NewQueryClient(conn), nil
+}
+
+func (s *ResolverService) DWNQuery() (dwnv1.QueryClient, error) {
+	conn, err := s.getClientConn()
+	if err != nil {
+		return nil, err
+	}
+	return dwnv1.NewQueryClient(conn), nil
+}
+
+func (s *ResolverService) SVCQuery() (svcv1.QueryClient, error) {
+	conn, err := s.getClientConn()
+	if err != nil {
+		return nil, err
+	}
+	return svcv1.NewQueryClient(conn), nil
 }
