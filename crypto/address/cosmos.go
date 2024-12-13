a205751602a073198c58108bd3976ee1e8491d75
Prad Nukala <prad@sonr.io>
2024-12-12 19:38:51 -0500

    refactor: move address packages to crypto/address

---
+++ crypto/address/cosmos.go
@@ -0,0 +1,12 @@
+package address
+
+type CosmosPrefix string
+
+const (
+	ATOMPrefix   CosmosPrefix = "cosmos"
+	AXELARPrefix CosmosPrefix = "axelar"
+	EVMOSPrefix  CosmosPrefix = "evmos"
+	OSMOPrefix   CosmosPrefix = "osmo"
+	SONRPrefix   CosmosPrefix = "idx"
+	STARSPrefix  CosmosPrefix = "stars"
+)
--- crypto/keys/pubkey.go
+++ crypto/keys/pubkey.go
@@ -1,6 +1,12 @@
 package keys
 
+import "github.com/onsonr/sonr/crypto/core/curves"
+
 type PubKey interface {
 	Type() string
 	Value() string
+}
+
+type mpcPubKey struct {
+	PublicPoint curves.Point
 }
