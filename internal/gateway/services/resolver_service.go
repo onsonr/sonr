cecf9d1e4b78938b0d4600a2971c9c768ebbf8a8
Prad Nukala <prad@sonr.io>
2024-12-14 11:55:00 -0500

    refactor: refactor grpc client creation and management

--- internal/gateway/services/resolver_service.go
+++ internal/gateway/services/resolver_service.go
@@ -1,14 +1,5 @@
 package services
 
-import (
-	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
-	didv1 "github.com/onsonr/sonr/api/did/v1"
-	dwnv1 "github.com/onsonr/sonr/api/dwn/v1"
-	svcv1 "github.com/onsonr/sonr/api/svc/v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
-
 type ResolverService struct {
 	grpcAddr string
 }
@@ -17,44 +8,4 @@ func NewResolverService(grpcAddr string) *ResolverService {
 	return &ResolverService{
 		grpcAddr: grpcAddr,
 	}
-}
-
-func (s *ResolverService) getClientConn() (*grpc.ClientConn, error) {
-	grpcConn, err := grpc.NewClient(s.grpcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		return nil, err
-	}
-	return grpcConn, nil
-}
-
-func (s *ResolverService) BankQuery() (bankv1beta1.QueryClient, error) {
-	conn, err := s.getClientConn()
-	if err != nil {
-		return nil, err
-	}
-	return bankv1beta1.NewQueryClient(conn), nil
-}
-
-func (s *ResolverService) DIDQuery() (didv1.QueryClient, error) {
-	conn, err := s.getClientConn()
-	if err != nil {
-		return nil, err
-	}
-	return didv1.NewQueryClient(conn), nil
-}
-
-func (s *ResolverService) DWNQuery() (dwnv1.QueryClient, error) {
-	conn, err := s.getClientConn()
-	if err != nil {
-		return nil, err
-	}
-	return dwnv1.NewQueryClient(conn), nil
-}
-
-func (s *ResolverService) SVCQuery() (svcv1.QueryClient, error) {
-	conn, err := s.getClientConn()
-	if err != nil {
-		return nil, err
-	}
-	return svcv1.NewQueryClient(conn), nil
 }
---
+++ pkg/common/clients.go
@@ -0,0 +1,59 @@
+package common
+
+import (
+	bankv1beta1 "cosmossdk.io/api/cosmos/bank/v1beta1"
+	nodev1beta1 "github.com/cosmos/cosmos-sdk/client/grpc/node"
+	didv1 "github.com/onsonr/sonr/api/did/v1"
+	dwnv1 "github.com/onsonr/sonr/api/dwn/v1"
+	svcv1 "github.com/onsonr/sonr/api/svc/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func conn(addr string) (*grpc.ClientConn, error) {
+	grpcConn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return grpcConn, nil
+}
+
+func BankQuery(addr string) (bankv1beta1.QueryClient, error) {
+	conn, err := conn(addr)
+	if err != nil {
+		return nil, err
+	}
+	return bankv1beta1.NewQueryClient(conn), nil
+}
+
+func DIDQuery(addr string) (didv1.QueryClient, error) {
+	conn, err := conn(addr)
+	if err != nil {
+		return nil, err
+	}
+	return didv1.NewQueryClient(conn), nil
+}
+
+func DWNQuery(addr string) (dwnv1.QueryClient, error) {
+	conn, err := conn(addr)
+	if err != nil {
+		return nil, err
+	}
+	return dwnv1.NewQueryClient(conn), nil
+}
+
+func NodeQuery(addr string) (nodev1beta1.ServiceClient, error) {
+	conn, err := conn(addr)
+	if err != nil {
+		return nil, err
+	}
+	return nodev1beta1.NewServiceClient(conn), nil
+}
+
+func SVCQuery(addr string) (svcv1.QueryClient, error) {
+	conn, err := conn(addr)
+	if err != nil {
+		return nil, err
+	}
+	return svcv1.NewQueryClient(conn), nil
+}
