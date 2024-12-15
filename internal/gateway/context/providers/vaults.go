a257819eedcd3dfe7cd72a04d4a4a85afc09fa0d
Prad Nukala <prad@sonr.io>
2024-12-14 20:12:10 -0500

    feat: implement vault creation API endpoint

--- internal/gateway/context/providers/vaults.go
+++ internal/gateway/context/providers/vaults.go
@@ -4,17 +4,20 @@ import (
 	"context"
 
 	"github.com/go-webauthn/webauthn/protocol"
+	"github.com/onsonr/sonr/crypto/mpc"
 	"github.com/onsonr/sonr/internal/gateway/views"
 	"github.com/onsonr/sonr/pkg/ipfsapi"
 )
 
 type VaultProvider interface {
-	New(ctx context.Context, handle string, origin string) (views.CreatePasskeyData, error)
+	New(ctx context.Context, handle string, origin string, challenge string) (views.CreatePasskeyData, error)
+	Push(ctx context.Context, handle string, origin string) (views.CreatePasskeyData, error)
 }
 
 type VaultService struct {
 	tokenStore     ipfsapi.IPFSTokenStore
 	challengeCache map[string]protocol.URLEncodedBase64
+	stagedEnclaves map[string]*mpc.Enclave
 }
 
 func NewVaultService(ipc ipfsapi.Client) VaultProvider {
@@ -24,6 +27,16 @@ func NewVaultService(ipc ipfsapi.Client) VaultProvider {
 	return svc
 }
 
-func (s *VaultService) New(ctx context.Context, handle string, origin string) (views.CreatePasskeyData, error) {
+func (s *VaultService) New(ctx context.Context, handle string, origin string, challenge string) (views.CreatePasskeyData, error) {
+	return views.CreatePasskeyData{
+		Address:       "",
+		Handle:        handle,
+		Name:          origin,
+		Challenge:     challenge,
+		CreationBlock: "00001",
+	}, nil
+}
+
+func (s *VaultService) Push(ctx context.Context, handle string, origin string) (views.CreatePasskeyData, error) {
 	return views.CreatePasskeyData{}, nil
 }
