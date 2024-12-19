52041fe3d642eb3c0e6f9d5c3125f85ce28132ef
Prad <prad@sonr.io>
2024-12-19 17:07:36 +0000

    chore(scripts): use sonr project and test config in doppler secrets

--- scripts/test_dop_node.sh
+++ scripts/test_dop_node.sh
@@ -8,13 +8,13 @@ cd $ROOT_DIR
 
 DOPPLER_TOKEN=$(skate get DOPPLER_NETWORK)
 
-ACC0=$(doppler secrets get KEY0_NAME --plain)
-ACC1=$(doppler secrets get KEY1_NAME --plain)
-MNEM0=$(doppler secrets get KEY0_MNEMONIC --plain)
-MNEM1=$(doppler secrets get KEY1_MNEMONIC --plain)
-CHAIN_ID=$(doppler secrets get CHAIN_ID --plain)
-TX_INDEX_INDEXER=$(doppler secrets get TX_INDEXER --plain)
-TX_INDEX_PSQL_CONN=$(doppler secrets get TX_PSQL_CONN --plain)
+ACC0=$(doppler secrets get KEY0_NAME --plain --project sonr --config test)
+ACC1=$(doppler secrets get KEY1_NAME --plain --project sonr --config test)
+MNEM0=$(doppler secrets get KEY0_MNEMONIC --plain --project sonr --config test)
+MNEM1=$(doppler secrets get KEY1_MNEMONIC --plain --project sonr --config test)
+CHAIN_ID=$(doppler secrets get CHAIN_ID --plain --project sonr --config test)
+TX_INDEX_INDEXER=$(doppler secrets get TX_INDEXER --plain --project sonr --config test)
+TX_INDEX_PSQL_CONN=$(doppler secrets get TX_PSQL_CONN --plain --project sonr --config test)
 
 # Run the node setup with all variables properly exported
 KEY0_NAME=$ACC0 KEY0_MNEMONIC=$MNEM0 KEY1_NAME=$ACC1 KEY1_MNEMONIC=$MNEM1 CHAIN_ID=$CHAIN_ID TX_INDEX_INDEXER=$TX_INDEX_INDEXER TX_INDEX_PSQL_CONN=$TX_INDEX_PSQL_CONN sh scripts/test_node.sh
