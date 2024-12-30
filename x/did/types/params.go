0a7b7d9fe20b7a8d9d16f114032d9895ef362f35
Prad Nukala <prad@sonr.io>
2024-12-30 12:31:55 -0500

    refactor: remove unnecessary seed message

--- x/did/types/params.go
+++ x/did/types/params.go
@@ -2,21 +2,11 @@ package types
 
 import (
 	"encoding/json"
-	fmt "fmt"
 )
 
 // DefaultParams returns default module parameters.
 func DefaultParams() Params {
 	return Params{}
-}
-
-// DefaultSeedMessage returns the default seed message
-func DefaultSeedMessage() string {
-	l1 := "The Sonr Network shall make no protocol that respects the establishment of centralized authority,"
-	l2 := "or prohibits the free exercise of decentralized identity; or abridges the freedom of data sovereignty,"
-	l3 := "or of encrypted communication; or the right of the users to peaceally interact and transact,"
-	l4 := "and to petition the Network for the redress of vulnerabilities."
-	return fmt.Sprintf("%s %s %s %s", l1, l2, l3, l4)
 }
 
 // Stringer method for Params.
