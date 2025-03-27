a1e555ccbaa4d03f134bd8858dcd5dd14ca4d314
Prad N <prad@didao.xyz>
2025-03-27 14:08:37 -0400

    feat: initialize default account capabilities for DID resolution

--- x/did/types/params.go
+++ x/did/types/params.go
@@ -6,7 +6,9 @@ import (
 
 // DefaultParams returns default module parameters.
 func DefaultParams() Params {
-	return Params{}
+	return Params{
+		Attenuations: DefaultAttenuations(),
+	}
 }
 
 // Stringer method for Params.
@@ -23,4 +25,42 @@ func (p Params) String() string {
 func (p Params) Validate() error {
 	// TODO:
 	return nil
+}
+
+// DefaultAttenuations returns the default Attenuation
+func DefaultAttenuations() []*Attenuation {
+	return []*Attenuation{
+		{
+			Resource: &Resource{
+				Kind:     "did",
+				Template: "{id}",
+			},
+			Capabilities: []*Capability{
+				{
+					Name:        "execute",
+					Parent:      "/account",
+					Command:     "/account/execute",
+					Description: "Execute a transaction on behalf of the account",
+				},
+				{
+					Name:        "link",
+					Parent:      "/account",
+					Command:     "/account/link",
+					Description: "Link a Verification Method to the account",
+				},
+				{
+					Name:        "unlink",
+					Parent:      "/account",
+					Command:     "/account/unlink",
+					Description: "Unlink a Verification Method from the account",
+				},
+				{
+					Name:        "authenticate",
+					Parent:      "/account",
+					Command:     "/account/authenticate",
+					Description: "Authenticate the account using a Verification Method",
+				},
+			},
+		},
+	}
 }
